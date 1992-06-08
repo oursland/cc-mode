fae4919f8ee88a589a7b18f5b17a3537b8952fee
warsaw <devnull@localhost>
1992-06-08 21:50:48 +0000

    removed extra define-key of DEL in c++-mode-map init

--- cc-mode.el
+++ cc-mode.el
@@ -6,8 +6,8 @@
 ;;                   and Stewart Clamen (clamen@cs.cmu.edu)
 ;;                  Done by fairly faithful modification of:
 ;;                  c-mode.el, Copyright (C) 1985 Richard M. Stallman.
-;; Last Modified:   $Date: 1992-06-08 21:47:11 $
-;; Version:         $Revision: 2.99 $
+;; Last Modified:   $Date: 1992-06-08 21:50:48 $
+;; Version:         $Revision: 2.100 $
 
 ;; Do a "C-h m" in a c++-mode buffer for more information on customizing
 ;; c++-mode.
@@ -43,7 +43,7 @@
 ;; LCD Archive Entry:
 ;; c++-mode|Barry A. Warsaw|c++-mode-help@anthem.nlm.nih.gov
 ;; |Mode for editing C++ code (was Detlefs' c++-mode.el)
-;; |$Date: 1992-06-08 21:47:11 $|$Revision: 2.99 $|
+;; |$Date: 1992-06-08 21:50:48 $|$Revision: 2.100 $|
 
 
 ;; ======================================================================
@@ -64,7 +64,6 @@
   (define-key c++-mode-map ";"         'c++-electric-semi)
   (define-key c++-mode-map "\e\C-h"    'mark-c-function)
   (define-key c++-mode-map "\e\C-q"    'c++-indent-exp)
-  (define-key c++-mode-map "\177"      'backward-delete-char-untabify)
   (define-key c++-mode-map "\t"        'c++-indent-command)
   (define-key c++-mode-map "\C-c\C-i"  'c++-insert-header)
   (define-key c++-mode-map "\C-c\C-\\" 'c++-macroize-region)
@@ -213,7 +212,7 @@ automatically escaped when typed in, but entering
 ;; c++-mode main entry point
 ;; ======================================================================
 (defun c++-mode ()
-  "Major mode for editing C++ code.  $Revision: 2.99 $
+  "Major mode for editing C++ code.  $Revision: 2.100 $
 Do a \"\\[describe-function] c++-dump-state\" for information on
 submitting bug reports.
 
@@ -1810,7 +1809,7 @@ function definition.")
 ;; ======================================================================
 ;; defuns for submitting bug reports
 ;; ======================================================================
-(defconst c++-version "$Revision: 2.99 $"
+(defconst c++-version "$Revision: 2.100 $"
   "c++-mode version number.")
 
 (defun c++-version ()
