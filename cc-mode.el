439804e9eb77c43f5fc8a596d802e9100aa0fb0d
warsaw <devnull@localhost>
1992-06-15 21:07:29 +0000

    fixed c++-indent-command when looking at a blank line and c++-tab-always-indent is nil

--- cc-mode.el
+++ cc-mode.el
@@ -6,8 +6,8 @@
 ;;                   and Stewart Clamen (clamen@cs.cmu.edu)
 ;;                  Done by fairly faithful modification of:
 ;;                  c-mode.el, Copyright (C) 1985 Richard M. Stallman.
-;; Last Modified:   $Date: 1992-06-15 14:17:09 $
-;; Version:         $Revision: 2.109 $
+;; Last Modified:   $Date: 1992-06-15 21:07:29 $
+;; Version:         $Revision: 2.110 $
 
 ;; Do a "C-h m" in a c++-mode buffer for more information on customizing
 ;; c++-mode.
@@ -43,7 +43,7 @@
 ;; LCD Archive Entry:
 ;; c++-mode|Barry A. Warsaw|c++-mode-help@anthem.nlm.nih.gov
 ;; |Mode for editing C++ code (was Detlefs' c++-mode.el)
-;; |$Date: 1992-06-15 14:17:09 $|$Revision: 2.109 $|
+;; |$Date: 1992-06-15 21:07:29 $|$Revision: 2.110 $|
 
 
 ;; ======================================================================
@@ -212,7 +212,7 @@ automatically escaped when typed in, but entering
 ;; c++-mode main entry point
 ;; ======================================================================
 (defun c++-mode ()
-  "Major mode for editing C++ code.  $Revision: 2.109 $
+  "Major mode for editing C++ code.  $Revision: 2.110 $
 Do a \"\\[describe-function] c++-dump-state\" for information on
 submitting bug reports.
 
@@ -801,7 +801,8 @@ of the expression are preserved."
 	(if (and (save-excursion
 		   (skip-chars-backward " \t")
 		   (bolp))
-		 (/= (point) (save-excursion (back-to-indentation) (point))))
+		 (or (looking-at "[ \t]*$")
+		     (/= (point) (c++-point-boi))))
 	    (c++-indent-line bod)
 	  (insert-tab)))
        ((eq c++-tab-always-indent t)
@@ -1583,6 +1584,12 @@ string according to mode's syntax."
     (c++-beginning-of-defun)
     (point)))
 
+(defun c++-point-boi ()
+  "Returns the value of point at back-to-indentation."
+  (save-excursion
+    (back-to-indentation)
+    (point)))
+
 
 ;; ======================================================================
 ;; defuns for "macroizations" -- making C++ parameterized types via macros
@@ -1819,7 +1826,7 @@ function definition.")
 ;; ======================================================================
 ;; defuns for submitting bug reports
 ;; ======================================================================
-(defconst c++-version "$Revision: 2.109 $"
+(defconst c++-version "$Revision: 2.110 $"
   "c++-mode version number.")
 
 (defun c++-version ()
