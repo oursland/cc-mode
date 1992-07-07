431ebc4c04fb1ae58d8a0de34dda818eb8556de4
warsaw <devnull@localhost>
1992-07-07 22:03:18 +0000

    grammer -> grammar in comment

--- cc-mode.el
+++ cc-mode.el
@@ -6,8 +6,8 @@
 ;;                   and Stewart Clamen (clamen@cs.cmu.edu)
 ;;                  Done by fairly faithful modification of:
 ;;                  c-mode.el, Copyright (C) 1985 Richard M. Stallman.
-;; Last Modified:   $Date: 1992-07-07 21:58:42 $
-;; Version:         $Revision: 2.133 $
+;; Last Modified:   $Date: 1992-07-07 22:03:18 $
+;; Version:         $Revision: 2.134 $
 
 ;; Do a "C-h m" in a c++-mode buffer for more information on customizing
 ;; c++-mode.
@@ -43,7 +43,7 @@
 ;; LCD Archive Entry:
 ;; c++-mode|Barry A. Warsaw|c++-mode-help@anthem.nlm.nih.gov
 ;; |Mode for editing C++ code (was Detlefs' c++-mode.el)
-;; |$Date: 1992-07-07 21:58:42 $|$Revision: 2.133 $|
+;; |$Date: 1992-07-07 22:03:18 $|$Revision: 2.134 $|
 
 
 ;; ======================================================================
@@ -246,7 +246,7 @@ Only currently supported behavior is '(alignleft).")
 ;; c++-mode main entry point
 ;; ======================================================================
 (defun c++-mode ()
-  "Major mode for editing C++ code.  $Revision: 2.133 $
+  "Major mode for editing C++ code.  $Revision: 2.134 $
 Do a \"\\[describe-function] c++-dump-state\" for information on
 submitting bug reports.
 
@@ -1799,7 +1799,7 @@ the leading \"// \" from each line, if any."
 	    (forward-line 1)))))
 
 ;; ======================================================================
-;; grammer pasing
+;; grammar pasing
 ;; ======================================================================
 
 ;;; Below are two regular expressions that attempt to match defuns
@@ -1951,7 +1951,7 @@ function definition.")
 ;; ======================================================================
 ;; defuns for submitting bug reports
 ;; ======================================================================
-(defconst c++-version "$Revision: 2.133 $"
+(defconst c++-version "$Revision: 2.134 $"
   "c++-mode version number.")
 
 (defun c++-version ()
