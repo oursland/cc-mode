74054a899c7cbf7d4186136ae65a736b0d726c82
warsaw <devnull@localhost>
1992-09-29 00:19:22 +0000

    replaced code accidently removed during last bug fix

--- cc-mode.el
+++ cc-mode.el
@@ -5,8 +5,8 @@
 ;;         1985 Richard M. Stallman
 ;; Maintainer: c++-mode-help@anthem.nlm.nih.gov
 ;; Created: a long, long, time ago. adapted from the original c-mode.el
-;; Version:         $Revision: 2.201 $
-;; Last Modified:   $Date: 1992-09-28 22:44:51 $
+;; Version:         $Revision: 2.202 $
+;; Last Modified:   $Date: 1992-09-29 00:19:22 $
 ;; Keywords: C++ C editing major-mode
 
 ;; Copyright (C) 1992 Free Software Foundation, Inc.
@@ -124,7 +124,7 @@
 ;; LCD Archive Entry:
 ;; c++-mode|Barry A. Warsaw|c++-mode-help@anthem.nlm.nih.gov
 ;; |Mode for editing C++ code (was Detlefs' c++-mode.el)
-;; |$Date: 1992-09-28 22:44:51 $|$Revision: 2.201 $|
+;; |$Date: 1992-09-29 00:19:22 $|$Revision: 2.202 $|
 
 ;;; Code:
 
@@ -376,7 +376,7 @@ Only currently supported behavior is '(alignleft).")
 ;; c++-mode main entry point
 ;; ======================================================================
 (defun c++-mode ()
-  "Major mode for editing C++ code.  $Revision: 2.201 $
+  "Major mode for editing C++ code.  $Revision: 2.202 $
 To submit a bug report, enter \"\\[c++-submit-bug-report]\"
 from a c++-mode buffer.
 
@@ -583,7 +583,7 @@ message."
    (memq c++-auto-hungry-initial-state '(hungry-only auto-hungry t))))
 
 (defun c++-c-mode ()
-  "Major mode for editing C code based on c++-mode. $Revision: 2.201 $
+  "Major mode for editing C code based on c++-mode. $Revision: 2.202 $
 Documentation for this mode is available by doing a
 \"\\[describe-function] c++-mode\"."
   (interactive)
@@ -1713,6 +1713,8 @@ BOD is the beginning of the C++ definition."
 	       (current-column)))
 	    (t
 	     ;; Statement.  Find previous non-comment character.
+	     (goto-char indent-point)
+	     (c++-backward-over-syntactic-ws containing-sexp)
 	     (if (not (memq char-before-ip '(nil ?\, ?\; ?} ?: ?\{)))
 		 ;; This line is continuation of preceding line's statement;
 		 ;; indent  c-continued-statement-offset  more than the
@@ -2203,7 +2205,7 @@ function definition.")
 ;; ======================================================================
 ;; defuns for submitting bug reports
 ;; ======================================================================
-(defconst c++-version "$Revision: 2.201 $"
+(defconst c++-version "$Revision: 2.202 $"
   "c++-mode version number.")
 
 (defun c++-version ()
