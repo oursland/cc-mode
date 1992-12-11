cb071dcc6b29b9d610c55750ee78229c2afbdab1
warsaw <devnull@localhost>
1992-12-11 16:26:05 +0000

    in electric terminator, don't just check for eolp to determine if terminator should electrify. also look for hanging whitespace which may look like eolp, even though it isn't:
            fooo();......\n
                  ^------^
    undiscovered ws---|

--- cc-mode.el
+++ cc-mode.el
@@ -5,8 +5,8 @@
 ;;         1985 Richard M. Stallman
 ;; Maintainer: c++-mode-help@anthem.nlm.nih.gov
 ;; Created: a long, long, time ago. adapted from the original c-mode.el
-;; Version:         $Revision: 2.238 $
-;; Last Modified:   $Date: 1992-12-10 22:59:25 $
+;; Version:         $Revision: 2.239 $
+;; Last Modified:   $Date: 1992-12-11 16:26:05 $
 ;; Keywords: C++ C editing major-mode
 
 ;; Copyright (C) 1992 Free Software Foundation, Inc.
@@ -124,7 +124,7 @@
 ;; LCD Archive Entry:
 ;; c++-mode|Barry A. Warsaw|c++-mode-help@anthem.nlm.nih.gov
 ;; |Mode for editing C++ code (was Detlefs' c++-mode.el)
-;; |$Date: 1992-12-10 22:59:25 $|$Revision: 2.238 $|
+;; |$Date: 1992-12-11 16:26:05 $|$Revision: 2.239 $|
 
 ;;; Code:
 
@@ -407,7 +407,7 @@ value is, though, the slower parts of c++-mode can become.")
 ;; c++-mode main entry point
 ;; ======================================================================
 (defun c++-mode ()
-  "Major mode for editing C++ code.  $Revision: 2.238 $
+  "Major mode for editing C++ code.  $Revision: 2.239 $
 To submit a bug report, enter \"\\[c++-submit-bug-report]\"
 from a c++-mode buffer.
 
@@ -615,7 +615,7 @@ message."
    (memq c++-auto-hungry-initial-state '(hungry-only auto-hungry t))))
 
 (defun c++-c-mode ()
-  "Major mode for editing C code based on c++-mode. $Revision: 2.238 $
+  "Major mode for editing C code based on c++-mode. $Revision: 2.239 $
 Documentation for this mode is available by doing a
 \"\\[describe-function] c++-mode\"."
   (interactive)
@@ -974,7 +974,10 @@ for member initialization list."
   "Insert character and correct line's indentation."
   (interactive "P")
   (let (insertpos (end (point)))
-    (if (and (not arg) (eolp)
+    (if (and (not arg)
+	     (save-excursion
+	       (skip-chars-forward " \t")
+	       (eolp))
 	     (not (save-excursion
 		    (beginning-of-line)
 		    (skip-chars-forward " \t")
@@ -2298,7 +2301,7 @@ function definition.")
 ;; ======================================================================
 ;; defuns for submitting bug reports
 ;; ======================================================================
-(defconst c++-version "$Revision: 2.238 $"
+(defconst c++-version "$Revision: 2.239 $"
   "c++-mode version number.")
 
 (defun c++-version ()
