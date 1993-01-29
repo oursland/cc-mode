51fb75d82c96ce3558a4fb2d217e4cf2b3f5ef34
warsaw <devnull@localhost>
1993-01-29 22:31:07 +0000

    in c++-forward/backward-sexp, only let bind parse-sexp-ignore-comments to t if emacs is fixed (not really fixed)

--- cc-mode.el
+++ cc-mode.el
@@ -5,8 +5,8 @@
 ;;         1985 Richard M. Stallman
 ;; Maintainer: c++-mode-help@anthem.nlm.nih.gov
 ;; Created: a long, long, time ago. adapted from the original c-mode.el
-;; Version:         $Revision: 2.269 $
-;; Last Modified:   $Date: 1993-01-27 22:48:56 $
+;; Version:         $Revision: 2.270 $
+;; Last Modified:   $Date: 1993-01-29 22:31:07 $
 ;; Keywords: C++ C editing major-mode
 
 ;; Copyright (C) 1992 Free Software Foundation, Inc.
@@ -131,7 +131,7 @@
 ;; LCD Archive Entry:
 ;; c++-mode|Barry A. Warsaw|c++-mode-help@anthem.nlm.nih.gov
 ;; |Mode for editing C++, and ANSI/K&R C code (was Detlefs' c++-mode.el)
-;; |$Date: 1993-01-27 22:48:56 $|$Revision: 2.269 $|
+;; |$Date: 1993-01-29 22:31:07 $|$Revision: 2.270 $|
 
 ;;; Code:
 
@@ -448,7 +448,7 @@ this variable to nil defeats backscan limits.")
 ;; c++-mode main entry point
 ;; ======================================================================
 (defun c++-mode ()
-  "Major mode for editing C++ code.  $Revision: 2.269 $
+  "Major mode for editing C++ code.  $Revision: 2.270 $
 To submit a bug report, enter \"\\[c++-submit-bug-report]\"
 from a c++-mode buffer.
 
@@ -669,7 +669,7 @@ message."
    (memq c++-auto-hungry-initial-state '(hungry-only auto-hungry t))))
 
 (defun c++-c-mode ()
-  "Major mode for editing K&R and ANSI C code. $Revision: 2.269 $
+  "Major mode for editing K&R and ANSI C code. $Revision: 2.270 $
 This mode is based on c++-mode. Documentation for this mode is
 available by doing a \"\\[describe-function] c++-mode\"."
   (interactive)
@@ -1363,12 +1363,12 @@ c++-untame-characters."
 
 (defun c++-forward-sexp (&optional arg)
   (interactive "p")
-  (let ((parse-sexp-ignore-comments t))
+  (let ((parse-sexp-ignore-comments c++-emacs-is-fixed-p))
     (forward-sexp arg)))
 
 (defun c++-backward-sexp (&optional arg)
   (interactive "p")
-  (let ((parse-sexp-ignore-comments t))
+  (let ((parse-sexp-ignore-comments c++-emacs-is-fixed-p))
     (backward-sexp arg)))
 
 
@@ -2436,7 +2436,7 @@ function definition.")
 ;; ======================================================================
 ;; defuns for submitting bug reports
 ;; ======================================================================
-(defconst c++-version "$Revision: 2.269 $"
+(defconst c++-version "$Revision: 2.270 $"
   "c++-mode version number.")
 
 (defun c++-version ()
