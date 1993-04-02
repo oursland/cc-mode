94b585c42606731b71fe29b7bae77de8eac7df7a
warsaw <devnull@localhost>
1993-04-02 15:35:10 +0000

    c++-backward-to-start-of-if wasn't taking into account "if" clauses in intervening comments.  Needed to add a c++-backward-syntactic-ws

--- cc-mode.el
+++ cc-mode.el
@@ -5,8 +5,8 @@
 ;;         1985 Richard M. Stallman
 ;; Maintainer: c++-mode-help@anthem.nlm.nih.gov
 ;; Created: a long, long, time ago. adapted from the original c-mode.el
-;; Version:         $Revision: 2.308 $
-;; Last Modified:   $Date: 1993-04-01 23:08:37 $
+;; Version:         $Revision: 2.309 $
+;; Last Modified:   $Date: 1993-04-02 15:35:10 $
 ;; Keywords: C++ C editing major-mode
 
 ;; Copyright (C) 1992, 1993 Free Software Foundation, Inc.
@@ -131,7 +131,7 @@
 ;; LCD Archive Entry:
 ;; c++-mode|Barry A. Warsaw|c++-mode-help@anthem.nlm.nih.gov
 ;; |Mode for editing C++, and ANSI/K&R C code (was Detlefs' c++-mode.el)
-;; |$Date: 1993-04-01 23:08:37 $|$Revision: 2.308 $|
+;; |$Date: 1993-04-02 15:35:10 $|$Revision: 2.309 $|
 
 ;;; Code:
 
@@ -458,7 +458,7 @@ this variable to nil defeats backscan limits.")
 ;; c++-mode main entry point
 ;; ======================================================================
 (defun c++-mode ()
-  "Major mode for editing C++ code.  $Revision: 2.308 $
+  "Major mode for editing C++ code.  $Revision: 2.309 $
 To submit a bug report, enter \"\\[c++-submit-bug-report]\"
 from a c++-mode buffer.
 
@@ -681,7 +681,7 @@ message."
    (memq c++-auto-hungry-initial-state '(hungry-only auto-hungry t))))
 
 (defun c++-c-mode ()
-  "Major mode for editing K&R and ANSI C code. $Revision: 2.308 $
+  "Major mode for editing K&R and ANSI C code. $Revision: 2.309 $
 This mode is based on c++-mode. Documentation for this mode is
 available by doing a \"\\[describe-function] c++-mode\"."
   (interactive)
@@ -2268,6 +2268,7 @@ optional LIM.  If LIM is ommitted, beginning-of-defun is used."
 	(limit (or limit (c++-point 'bod))))
     (while (and (not (bobp))
 		(not (zerop if-level)))
+      (c++-backward-syntactic-ws)
       (c++-backward-sexp 1)
       (cond ((looking-at "else\\b")
 	     (setq if-level (1+ if-level)))
@@ -2544,7 +2545,7 @@ function definition.")
 ;; ======================================================================
 ;; defuns for submitting bug reports
 ;; ======================================================================
-(defconst c++-version "$Revision: 2.308 $"
+(defconst c++-version "$Revision: 2.309 $"
   "c++-mode version number.")
 
 (defun c++-version ()
