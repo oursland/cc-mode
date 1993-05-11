e0b60558ef56a674a4c81d2fdb52c8eccbf9179b
warsaw <devnull@localhost>
1993-05-11 13:44:31 +0000

    fixed the string-match call in c++-emacs-features

--- cc-mode.el
+++ cc-mode.el
@@ -5,8 +5,8 @@
 ;;         1985 Richard M. Stallman
 ;; Maintainer: c++-mode-help@anthem.nlm.nih.gov
 ;; Created: a long, long, time ago. adapted from the original c-mode.el
-;; Version:         $Revision: 2.321 $
-;; Last Modified:   $Date: 1993-05-10 18:20:42 $
+;; Version:         $Revision: 2.322 $
+;; Last Modified:   $Date: 1993-05-11 13:44:31 $
 ;; Keywords: C++ C editing major-mode
 
 ;; Copyright (C) 1992, 1993 Free Software Foundation, Inc.
@@ -132,7 +132,7 @@
 ;; LCD Archive Entry:
 ;; c++-mode|Barry A. Warsaw|c++-mode-help@anthem.nlm.nih.gov
 ;; |Mode for editing C++, and ANSI/K&R C code (was Detlefs' c++-mode.el)
-;; |$Date: 1993-05-10 18:20:42 $|$Revision: 2.321 $|
+;; |$Date: 1993-05-11 13:44:31 $|$Revision: 2.322 $|
 
 ;;; Code:
 
@@ -150,7 +150,7 @@
 (defconst c++-emacs-features
   (list
    (if (= 8 (length (parse-partial-sexp (point) (point))))
-       (if (or (string-match emacs-version "Lucid")
+       (if (or (string-match "Lucid" emacs-version)
 	       (fboundp 'forward-syntactic-ws))
 	   '8-bit '1-bit)
      'no-dual-comments)
@@ -465,7 +465,7 @@ this variable to nil defeats backscan limits.")
 ;; c++-mode main entry point
 ;; ======================================================================
 (defun c++-mode ()
-  "Major mode for editing C++ code.  $Revision: 2.321 $
+  "Major mode for editing C++ code.  $Revision: 2.322 $
 To submit a bug report, enter \"\\[c++-submit-bug-report]\"
 from a c++-mode buffer.
 
@@ -686,7 +686,7 @@ message."
    (memq c++-auto-hungry-initial-state '(hungry-only auto-hungry t))))
 
 (defun c++-c-mode ()
-  "Major mode for editing K&R and ANSI C code. $Revision: 2.321 $
+  "Major mode for editing K&R and ANSI C code. $Revision: 2.322 $
 This mode is based on c++-mode. Documentation for this mode is
 available by doing a \"\\[describe-function] c++-mode\"."
   (interactive)
@@ -2611,7 +2611,7 @@ function definition.")
 ;; ======================================================================
 ;; defuns for submitting bug reports
 ;; ======================================================================
-(defconst c++-version "$Revision: 2.321 $"
+(defconst c++-version "$Revision: 2.322 $"
   "c++-mode version number.")
 (defconst c++-mode-help-address "c++-mode-help@anthem.nlm.nih.gov"
   "Address accepting submission of bug reports.")
