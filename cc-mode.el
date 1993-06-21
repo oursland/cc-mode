29f4254de090f37e5cc7ecb26f66ee4f9107f3ae
warsaw <devnull@localhost>
1993-06-21 21:40:15 +0000

    should not skip-chars-backward in ws-2, let forward-comment handle that

--- cc-mode.el
+++ cc-mode.el
@@ -5,8 +5,8 @@
 ;;         1985 Richard M. Stallman
 ;; Maintainer: c++-mode-help@anthem.nlm.nih.gov
 ;; Created: a long, long, time ago. adapted from the original c-mode.el
-;; Version:         $Revision: 2.351 $
-;; Last Modified:   $Date: 1993-06-18 21:31:24 $
+;; Version:         $Revision: 2.352 $
+;; Last Modified:   $Date: 1993-06-21 21:40:15 $
 ;; Keywords: C++ C editing major-mode
 
 ;; Copyright (C) 1992, 1993 Free Software Foundation, Inc.
@@ -132,7 +132,7 @@
 ;; LCD Archive Entry:
 ;; c++-mode|Barry A. Warsaw|c++-mode-help@anthem.nlm.nih.gov
 ;; |Mode for editing C++, and ANSI/K&R C code (was Detlefs' c++-mode.el)
-;; |$Date: 1993-06-18 21:31:24 $|$Revision: 2.351 $|
+;; |$Date: 1993-06-21 21:40:15 $|$Revision: 2.352 $|
 
 ;;; Code:
 
@@ -487,7 +487,7 @@ this variable to nil defeats backscan limits.")
 ;; c++-mode main entry point
 ;; ======================================================================
 (defun c++-mode ()
-  "Major mode for editing C++ code.  $Revision: 2.351 $
+  "Major mode for editing C++ code.  $Revision: 2.352 $
 To submit a problem report, enter `\\[c++-submit-bug-report]' from a
 c++-mode buffer.  This automatically sets up a mail buffer with
 version information already added.  You just need to add a description
@@ -704,7 +704,7 @@ no args, if that value is non-nil."
    (memq c++-auto-hungry-initial-state '(hungry-only auto-hungry t))))
 
 (defun c++-c-mode ()
-  "Major mode for editing K&R and ANSI C code.  $Revision: 2.351 $
+  "Major mode for editing K&R and ANSI C code.  $Revision: 2.352 $
 This mode is based on c++-mode.  Documentation for this mode is
 available by doing a `\\[describe-function] c++-mode'."
   (interactive)
@@ -1617,8 +1617,7 @@ optional LIM.  If LIM is omitted, `beginning-of-defun' is used."
 		(modify-syntax-entry ?# "< b" c++-mode-syntax-table)
 		(while (/= here (point))
 		  (setq here (point))
-		  (forward-comment -1)
-		  (skip-chars-backward " \t\n")))
+		  (forward-comment -1)))
 	    (modify-syntax-entry ?# "." c++-mode-syntax-table)))
       )))
 
@@ -2770,7 +2769,7 @@ definition.")
 ;; ======================================================================
 ;; defuns for submitting bug reports
 ;; ======================================================================
-(defconst c++-version "$Revision: 2.351 $"
+(defconst c++-version "$Revision: 2.352 $"
   "c++-mode version number.")
 (defconst c++-mode-help-address "c++-mode-help@anthem.nlm.nih.gov"
   "Address accepting submission of bug reports.")
