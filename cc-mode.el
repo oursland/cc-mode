ffeeb496d8bfddb8a9a8c6c654c1d0bbb51f40b9
warsaw <devnull@localhost>
1993-09-24 21:37:30 +0000

    (c++-calculate-indent): removed the commented out text which used to test for null state.  we never hit null state from c++-parse-state anymore.

--- cc-mode.el
+++ cc-mode.el
@@ -5,8 +5,8 @@
 ;;         1985 Richard M. Stallman
 ;; Maintainer: c++-mode-help@anthem.nlm.nih.gov
 ;; Created: a long, long, time ago. adapted from the original c-mode.el
-;; Version:         $Revision: 3.7 $
-;; Last Modified:   $Date: 1993-09-24 21:20:54 $
+;; Version:         $Revision: 3.8 $
+;; Last Modified:   $Date: 1993-09-24 21:37:30 $
 ;; Keywords: C++ C editing major-mode
 
 ;; Copyright (C) 1992, 1993 Free Software Foundation, Inc.
@@ -124,7 +124,7 @@
 ;; LCD Archive Entry:
 ;; c++-mode|Barry A. Warsaw|c++-mode-help@anthem.nlm.nih.gov
 ;; |Mode for editing C++, and ANSI/K&R C code (was Detlefs' c++-mode.el)
-;; |$Date: 1993-09-24 21:20:54 $|$Revision: 3.7 $|
+;; |$Date: 1993-09-24 21:37:30 $|$Revision: 3.8 $|
 
 ;;; Code:
 
@@ -470,7 +470,7 @@ this variable to nil defeats backscan limits.")
 ;; c++-mode main entry point
 ;; ======================================================================
 (defun c++-mode ()
-  "Major mode for editing C++ code.  $Revision: 3.7 $
+  "Major mode for editing C++ code.  $Revision: 3.8 $
 To submit a problem report, enter `\\[c++-submit-bug-report]' from a
 c++-mode buffer.  This automatically sets up a mail buffer with
 version information already added.  You just need to add a description
@@ -702,7 +702,7 @@ no args, if that value is non-nil."
    (memq c++-auto-hungry-initial-state '(hungry-only auto-hungry t))))
 
 (defun c++-c-mode ()
-  "Major mode for editing K&R and ANSI C code.  $Revision: 3.7 $
+  "Major mode for editing K&R and ANSI C code.  $Revision: 3.8 $
 This mode is based on c++-mode.  Documentation for this mode is
 available by doing a `\\[describe-function] c++-mode'."
   (interactive)
@@ -1990,20 +1990,8 @@ BOD is the beginning of the C++ definition."
 	(goto-char bod))
       (setq parse-start (point)
 	    state (c++-parse-state indent-point)
-	    containing-sexp (nth 1 state))
-      ;; it is possible that c++-defun-header-weak could not find the
-      ;; beginning of the C++ definition. The following code attempts
-      ;; to work around this.  It is probably better to just use
-      ;; c++-match-header-strongly, but there are performance questions
-;;      (if (null state)
-;;	  (error "null state!"))
-;;	  (let* ((c++-match-header-strongly t)
-;;		 (bod (c++-point 'bod)))
-;;	    (goto-char bod)
-;;	    (setq state (c++-parse-state indent-point)
-;;		  containing-sexp (nth 1 state)
-;;		  parse-start (point))))
-      (setq literal (c++-in-literal bod))
+	    containing-sexp (nth 1 state)
+	    literal (c++-in-literal bod))
       ;; cache char before indent point
       (save-excursion
 	(goto-char indent-point)
@@ -2613,7 +2601,7 @@ the leading `// ' from each line, if any."
 ;; ======================================================================
 ;; defuns for submitting bug reports
 ;; ======================================================================
-(defconst c++-version "$Revision: 3.7 $"
+(defconst c++-version "$Revision: 3.8 $"
   "c++-mode version number.")
 (defconst c++-mode-help-address "c++-mode-help@anthem.nlm.nih.gov"
   "Address accepting submission of bug reports.")
