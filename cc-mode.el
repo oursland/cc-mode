0e229657b18fd9a0c1e849ec15af987f5be1108a
warsaw <devnull@localhost>
1993-09-24 22:01:18 +0000

    (c++-calculate-indent): when inside a class definition, if we are looking at an open brace, we apply c-continued-brace-offset instead of just zero.  this allows users to indent the opening brace of an inline function body.

--- cc-mode.el
+++ cc-mode.el
@@ -5,8 +5,8 @@
 ;;         1985 Richard M. Stallman
 ;; Maintainer: c++-mode-help@anthem.nlm.nih.gov
 ;; Created: a long, long, time ago. adapted from the original c-mode.el
-;; Version:         $Revision: 3.8 $
-;; Last Modified:   $Date: 1993-09-24 21:37:30 $
+;; Version:         $Revision: 3.9 $
+;; Last Modified:   $Date: 1993-09-24 22:01:18 $
 ;; Keywords: C++ C editing major-mode
 
 ;; Copyright (C) 1992, 1993 Free Software Foundation, Inc.
@@ -124,7 +124,7 @@
 ;; LCD Archive Entry:
 ;; c++-mode|Barry A. Warsaw|c++-mode-help@anthem.nlm.nih.gov
 ;; |Mode for editing C++, and ANSI/K&R C code (was Detlefs' c++-mode.el)
-;; |$Date: 1993-09-24 21:37:30 $|$Revision: 3.8 $|
+;; |$Date: 1993-09-24 22:01:18 $|$Revision: 3.9 $|
 
 ;;; Code:
 
@@ -470,7 +470,7 @@ this variable to nil defeats backscan limits.")
 ;; c++-mode main entry point
 ;; ======================================================================
 (defun c++-mode ()
-  "Major mode for editing C++ code.  $Revision: 3.8 $
+  "Major mode for editing C++ code.  $Revision: 3.9 $
 To submit a problem report, enter `\\[c++-submit-bug-report]' from a
 c++-mode buffer.  This automatically sets up a mail buffer with
 version information already added.  You just need to add a description
@@ -702,7 +702,7 @@ no args, if that value is non-nil."
    (memq c++-auto-hungry-initial-state '(hungry-only auto-hungry t))))
 
 (defun c++-c-mode ()
-  "Major mode for editing K&R and ANSI C code.  $Revision: 3.8 $
+  "Major mode for editing K&R and ANSI C code.  $Revision: 3.9 $
 This mode is based on c++-mode.  Documentation for this mode is
 available by doing a `\\[describe-function] c++-mode'."
   (interactive)
@@ -2037,7 +2037,7 @@ BOD is the beginning of the C++ definition."
 		 (progn
 		   (c++-backward-syntactic-ws parse-start)
 		   (bobp)))
-	     0)
+	     c-continued-brace-offset)
 	    ;; first arg decl or member init
 	    ((c++-in-function-p)
 	     (goto-char indent-point)
@@ -2601,7 +2601,7 @@ the leading `// ' from each line, if any."
 ;; ======================================================================
 ;; defuns for submitting bug reports
 ;; ======================================================================
-(defconst c++-version "$Revision: 3.8 $"
+(defconst c++-version "$Revision: 3.9 $"
   "c++-mode version number.")
 (defconst c++-mode-help-address "c++-mode-help@anthem.nlm.nih.gov"
   "Address accepting submission of bug reports.")
