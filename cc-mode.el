e914c44b18104ca1df28519d365f4a2f1164cbf0
warsaw <devnull@localhost>
1993-10-27 14:28:16 +0000

    (c++-common-init): don't set the auto-hungry state here since the mode hook has not been run yet.
    (c++-mode) (c++-c-mode): set the auto-hungry state for the buffer
    after the mode hook has been run

--- cc-mode.el
+++ cc-mode.el
@@ -5,8 +5,8 @@
 ;;         1985 Richard M. Stallman
 ;; Maintainer: c++-mode-help@anthem.nlm.nih.gov
 ;; Created: a long, long, time ago. adapted from the original c-mode.el
-;; Version:         $Revision: 3.32 $
-;; Last Modified:   $Date: 1993-10-20 18:46:53 $
+;; Version:         $Revision: 3.33 $
+;; Last Modified:   $Date: 1993-10-27 14:28:16 $
 ;; Keywords: C++ C editing major-mode
 
 ;; Copyright (C) 1992, 1993 Free Software Foundation, Inc.
@@ -124,7 +124,7 @@
 ;; LCD Archive Entry:
 ;; c++-mode|Barry A. Warsaw|c++-mode-help@anthem.nlm.nih.gov
 ;; |Mode for editing C++, and ANSI/K&R C code (was Detlefs' c++-mode.el)
-;; |$Date: 1993-10-20 18:46:53 $|$Revision: 3.32 $|
+;; |$Date: 1993-10-27 14:28:16 $|$Revision: 3.33 $|
 
 ;;; Code:
 
@@ -534,12 +534,10 @@ this variable to nil defeats backscan limits.")
 				  (list element))))))
 	       mode-line-format)
 	      modeline)))
-  (c++-set-auto-hungry-state
-   (memq c++-auto-hungry-initial-state '(auto-only   auto-hungry t))
-   (memq c++-auto-hungry-initial-state '(hungry-only auto-hungry t))))
+  )
 
 (defun c++-mode ()
-  "Major mode for editing C++ code.  $Revision: 3.32 $
+  "Major mode for editing C++ code.  $Revision: 3.33 $
 To submit a problem report, enter `\\[c++-submit-bug-report]' from a
 c++-mode buffer.  This automatically sets up a mail buffer with
 version information already added.  You just need to add a description
@@ -728,10 +726,13 @@ no args, if that value is non-nil."
   ;; now set their values
   (setq comment-start "// "
 	comment-end "")
-  (run-hooks 'c++-mode-hook))
+  (run-hooks 'c++-mode-hook)
+  (c++-set-auto-hungry-state
+   (memq c++-auto-hungry-initial-state '(auto-only   auto-hungry t))
+   (memq c++-auto-hungry-initial-state '(hungry-only auto-hungry t))))
 
 (defun c++-c-mode ()
-  "Major mode for editing K&R and ANSI C code.  $Revision: 3.32 $
+  "Major mode for editing K&R and ANSI C code.  $Revision: 3.33 $
 This mode is based on c++-mode.  Documentation for this mode is
 available by doing a `\\[describe-function] c++-mode'.  Only real
 difference is that this sets up the buffer for editing C code, and it
@@ -744,7 +745,10 @@ runs the hook `c++-c-mode-hook' instead of `c++-mode-hook'."
 	local-abbrev-table c-mode-abbrev-table)
   (setq comment-start "/* "
 	comment-end   " */")
-  (run-hooks 'c++-c-mode-hook))
+  (run-hooks 'c++-c-mode-hook)
+  (c++-set-auto-hungry-state
+   (memq c++-auto-hungry-initial-state '(auto-only   auto-hungry t))
+   (memq c++-auto-hungry-initial-state '(hungry-only auto-hungry t))))
 
 (defun c++-comment-indent ()
   "Used by `indent-for-comment' to decide how much to indent a comment
@@ -2658,7 +2662,7 @@ the leading `// ' from each line, if any."
 ;; ======================================================================
 ;; defuns for submitting bug reports
 
-(defconst c++-version "$Revision: 3.32 $"
+(defconst c++-version "$Revision: 3.33 $"
   "c++-mode version number.")
 (defconst c++-mode-help-address "c++-mode-help@anthem.nlm.nih.gov"
   "Address accepting submission of bug reports.")
