ffd0e6cd24890f742454b53e43aa4c87110884d7
warsaw <devnull@localhost>
1993-11-20 21:22:42 +0000

    (cc-setup-comment-indent-variable) (cc-keep-region-active): make these defuns so that the .elc file won't be editor version specific. The trade off is that the byte compiler will complain

--- cc-mode.el
+++ cc-mode.el
@@ -5,8 +5,8 @@
 ;;          1985 Richard M. Stallman
 ;; Maintainer: cc-mode-help@anthem.nlm.nih.gov
 ;; Created: a long, long, time ago. adapted from the original c-mode.el
-;; Version:         $Revision: 3.73 $
-;; Last Modified:   $Date: 1993-11-20 21:08:25 $
+;; Version:         $Revision: 3.74 $
+;; Last Modified:   $Date: 1993-11-20 21:22:42 $
 ;; Keywords: C++ C editing major-mode
 
 ;; Copyright (C) 1992, 1993 Free Software Foundation, Inc.
@@ -67,7 +67,7 @@
 ;; LCD Archive Entry:
 ;; cc-mode|Barry A. Warsaw|cc-mode-help@anthem.nlm.nih.gov
 ;; |Major mode for editing C++, and ANSI/K&R C code
-;; |$Date: 1993-11-20 21:08:25 $|$Revision: 3.73 $|
+;; |$Date: 1993-11-20 21:22:42 $|$Revision: 3.74 $|
 
 ;;; Code:
 
@@ -488,7 +488,7 @@ that users are familiar with.")
 
 ;; main entry points for the modes
 (defun cc-c++-mode ()
-  "Major mode for editing C++ code.  $Revision: 3.73 $
+  "Major mode for editing C++ code.  $Revision: 3.74 $
 To submit a problem report, enter `\\[cc-submit-bug-report]' from a
 cc-c++-mode buffer.  This automatically sets up a mail buffer with
 version information already added.  You just need to add a description
@@ -518,7 +518,7 @@ Key bindings:
    (memq cc-auto-hungry-initial-state '(hungry-only auto-hungry t))))
 
 (defun cc-c-mode ()
-  "Major mode for editing K&R and ANSI C code.  $Revision: 3.73 $
+  "Major mode for editing K&R and ANSI C code.  $Revision: 3.74 $
 To submit a problem report, enter `\\[cc-submit-bug-report]' from a
 cc-c-mode buffer.  This automatically sets up a mail buffer with
 version information already added.  You just need to add a description
@@ -547,16 +547,14 @@ Key bindings:
    (memq cc-auto-hungry-initial-state '(auto-only   auto-hungry t))
    (memq cc-auto-hungry-initial-state '(hungry-only auto-hungry t))))
 
-(defmacro cc-setup-comment-indent-variable ()
-  ;; shut the byte compiler up
+(defun cc-setup-comment-indent-variable ()
+  ;; ignore any byte compiler warnings you might get here
   (if (boundp 'comment-indent-function)
-      (` (progn
+      (progn
 	   (make-local-variable 'comment-indent-function)
-	   (setq comment-indent-function 'cc-comment-indent)))
-    (` (progn
-	 (make-local-variable 'comment-indent-hook)
-	 (setq comment-indent-hook 'cc-comment-indent)))
-    ))
+	   (setq comment-indent-function 'cc-comment-indent))
+    (make-local-variable 'comment-indent-hook)
+    (setq comment-indent-hook 'cc-comment-indent)))
 
 (defun cc-common-init ()
   ;; Common initializations for cc-c++-mode and cc-c-mode.
@@ -636,14 +634,13 @@ Key bindings:
 
 
 ;; auto-newline/hungry delete key
-(defmacro cc-keep-region-active ()
-  ;; macro to keep region active in Emacs 19
-  (cond
-   ((boundp 'zmacs-region-stays)
-    (` (if (interactive-p) (setq zmacs-region-stays t))))
-   ((boundp 'deactivate-mark)
-    (` (if (interactive-p) (setq deactivate-mark (not mark-active)))))
-   ))
+(defun cc-keep-region-active ()
+  ;; macro to keep region active in Emacs 19. Ignore any byte-compiler
+  ;; warnings you might see
+  (if (boundp 'zmacs-region-stays)
+      (setq zmacs-region-stays t)
+   (if (boundp 'deactivate-mark)
+       (setq deactivate-mark (not mark-active)))))
 
 (defun cc-set-auto-hungry-state (auto-p hungry-p)
   ;; Set auto/hungry to state indicated by AUTO-P and HUNGRY-P, and
@@ -2322,7 +2319,7 @@ the leading `// ' from each line, if any."
 
 ;; defuns for submitting bug reports
 
-(defconst cc-version "$Revision: 3.73 $"
+(defconst cc-version "$Revision: 3.74 $"
   "cc-mode version number.")
 (defconst cc-mode-help-address "cc-mode-help@anthem.nlm.nih.gov"
   "Address accepting submission of bug reports.")
