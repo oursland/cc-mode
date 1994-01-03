9334cfc77e65d0e56c4a096378823f03fc9e3b63
warsaw <devnull@localhost>
1994-01-03 23:00:14 +0000

    (c-beginning-of-statement-1): before calculating last-begin, skip over any bogus characters like *&:. for the following situations:
        if(::foo(),
        if(.0123,
        if(&blatz,
        if(*bar,

--- cc-mode.el
+++ cc-mode.el
@@ -5,8 +5,8 @@
 ;;          1985 Richard M. Stallman
 ;; Maintainer: cc-mode-help@anthem.nlm.nih.gov
 ;; Created: a long, long, time ago. adapted from the original c-mode.el
-;; Version:         $Revision: 3.163 $
-;; Last Modified:   $Date: 1994-01-03 19:39:37 $
+;; Version:         $Revision: 3.164 $
+;; Last Modified:   $Date: 1994-01-03 23:00:14 $
 ;; Keywords: C++ C editing major-mode
 
 ;; Copyright (C) 1992, 1993 Free Software Foundation, Inc.
@@ -79,7 +79,7 @@
 ;; LCD Archive Entry:
 ;; cc-mode.el|Barry A. Warsaw|cc-mode-help@anthem.nlm.nih.gov
 ;; |Major mode for editing C++, and ANSI/K&R C code
-;; |$Date: 1994-01-03 19:39:37 $|$Revision: 3.163 $|
+;; |$Date: 1994-01-03 23:00:14 $|$Revision: 3.164 $|
 
 ;;; Code:
 
@@ -649,7 +649,7 @@ behavior that users are familiar with.")
 ;;;###autoload
 (defun c++-mode ()
   "Major mode for editing C++ code.
-CC-MODE REVISION: $Revision: 3.163 $
+CC-MODE REVISION: $Revision: 3.164 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c++-mode buffer.  This automatically sets up a mail buffer with
 version information already added.  You just need to add a description
@@ -683,7 +683,7 @@ Key bindings:
 ;;;###autoload
 (defun c-mode ()
   "Major mode for editing K&R and ANSI C code.
-CC-MODE REVISION: $Revision: 3.163 $
+CC-MODE REVISION: $Revision: 3.164 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c-mode buffer.  This automatically sets up a mail buffer with version
 information already added.  You just need to add a description of the
@@ -1469,6 +1469,8 @@ search."
 			    ))
 		      (not (looking-at c-conditional-key))
 		      )
+	    ;; this seems bogus, but necessary
+	    (skip-chars-backward "*&:.")
 	    (if (not (looking-at c-label-key))
 		(setq last-begin (point)
 		      first nil)))
@@ -2951,7 +2953,7 @@ region."
 
 ;; defuns for submitting bug reports
 
-(defconst c-version "$Revision: 3.163 $"
+(defconst c-version "$Revision: 3.164 $"
   "cc-mode version number.")
 (defconst c-mode-help-address "cc-mode-help@anthem.nlm.nih.gov"
   "Address accepting submission of bug reports.")
