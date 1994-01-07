3791ddff32dd0b6fafcfe895a773fb26c485ae33
warsaw <devnull@localhost>
1994-01-07 23:27:20 +0000

    (c-beginning-of-statement-1): do not let backward scanning stop inside a literal.  use c-in-literal to stop this, which really catches only preprocs

--- cc-mode.el
+++ cc-mode.el
@@ -5,8 +5,8 @@
 ;;          1985 Richard M. Stallman
 ;; Maintainer: cc-mode-help@anthem.nlm.nih.gov
 ;; Created: a long, long, time ago. adapted from the original c-mode.el
-;; Version:         $Revision: 3.169 $
-;; Last Modified:   $Date: 1994-01-07 23:16:54 $
+;; Version:         $Revision: 3.170 $
+;; Last Modified:   $Date: 1994-01-07 23:27:20 $
 ;; Keywords: C++ C editing major-mode
 
 ;; Copyright (C) 1992, 1993, 1994 Free Software Foundation, Inc.
@@ -82,7 +82,7 @@
 ;; LCD Archive Entry:
 ;; cc-mode.el|Barry A. Warsaw|cc-mode-help@anthem.nlm.nih.gov
 ;; |Major mode for editing C++, and ANSI/K&R C code
-;; |$Date: 1994-01-07 23:16:54 $|$Revision: 3.169 $|
+;; |$Date: 1994-01-07 23:27:20 $|$Revision: 3.170 $|
 
 ;;; Code:
 
@@ -652,7 +652,7 @@ behavior that users are familiar with.")
 ;;;###autoload
 (defun c++-mode ()
   "Major mode for editing C++ code.
-CC-MODE REVISION: $Revision: 3.169 $
+CC-MODE REVISION: $Revision: 3.170 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c++-mode buffer.  This automatically sets up a mail buffer with
 version information already added.  You just need to add a description
@@ -686,7 +686,7 @@ Key bindings:
 ;;;###autoload
 (defun c-mode ()
   "Major mode for editing K&R and ANSI C code.
-CC-MODE REVISION: $Revision: 3.169 $
+CC-MODE REVISION: $Revision: 3.170 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c-mode buffer.  This automatically sets up a mail buffer with version
 information already added.  You just need to add a description of the
@@ -1470,11 +1470,13 @@ search."
 			(or first
 			    (not (re-search-forward "[;{}]" last-begin t))
 			    ))
-		      (not (looking-at c-conditional-key))
+		      (not (or (c-in-literal)
+			       (looking-at c-conditional-key)))
 		      )
 	    ;; this seems bogus, but necessary
 	    (skip-chars-backward "*&:.")
-	    (if (not (looking-at c-label-key))
+	    (if (and (not (c-in-literal))
+		     (not (looking-at c-label-key)))
 		(setq last-begin (point)
 		      first nil)))
 	  (cond
@@ -2967,7 +2969,7 @@ region."
 
 ;; defuns for submitting bug reports
 
-(defconst c-version "$Revision: 3.169 $"
+(defconst c-version "$Revision: 3.170 $"
   "cc-mode version number.")
 (defconst c-mode-help-address "cc-mode-help@anthem.nlm.nih.gov"
   "Address accepting submission of bug reports.")
