401ebeef444f1e23a35fba3b6a7fa7ea2cf9ef2c
warsaw <devnull@localhost>
1994-01-12 00:31:24 +0000

    (c-beginning-of-statement): when in a comment, or looking at the beginning of a comment, only call forward-sentence if not called interactively.  Thus programmatically, this function always goes back to the real beginning-of-statement.

--- cc-mode.el
+++ cc-mode.el
@@ -5,8 +5,8 @@
 ;;          1985 Richard M. Stallman
 ;; Maintainer: cc-mode-help@anthem.nlm.nih.gov
 ;; Created: a long, long, time ago. adapted from the original c-mode.el
-;; Version:         $Revision: 3.188 $
-;; Last Modified:   $Date: 1994-01-12 00:20:44 $
+;; Version:         $Revision: 3.189 $
+;; Last Modified:   $Date: 1994-01-12 00:31:24 $
 ;; Keywords: C++ C editing major-mode
 
 ;; Copyright (C) 1992, 1993, 1994 Barry A. Warsaw
@@ -85,7 +85,7 @@
 ;; LCD Archive Entry:
 ;; cc-mode.el|Barry A. Warsaw|cc-mode-help@anthem.nlm.nih.gov
 ;; |Major mode for editing C++, and ANSI/K&R C code
-;; |$Date: 1994-01-12 00:20:44 $|$Revision: 3.188 $|
+;; |$Date: 1994-01-12 00:31:24 $|$Revision: 3.189 $|
 
 ;;; Code:
 
@@ -685,7 +685,7 @@ behavior that users are familiar with.")
 ;;;###autoload
 (defun c++-mode ()
   "Major mode for editing C++ code.
-cc-mode Revision: $Revision: 3.188 $
+cc-mode Revision: $Revision: 3.189 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c++-mode buffer.  This automatically sets up a mail buffer with
 version information already added.  You just need to add a description
@@ -716,7 +716,7 @@ Key bindings:
 ;;;###autoload
 (defun c-mode ()
   "Major mode for editing K&R and ANSI C code.
-cc-mode Revision: $Revision: 3.188 $
+cc-mode Revision: $Revision: 3.189 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c-mode buffer.  This automatically sets up a mail buffer with version
 information already added.  You just need to add a description of the
@@ -1429,13 +1429,14 @@ search."
     (save-excursion
       (goto-char lim)
       (setq state (parse-partial-sexp (point) here nil nil)))
-    (if (or (nth 3 state)
-	    (nth 4 state)
-	    (looking-at (concat "[ \t]*" comment-start-skip))
-	    (save-excursion
-	      (skip-chars-backward " \t")
-	      (goto-char (- (point) 2))
-	      (looking-at "\\*/")))
+    (if (and (interactive-p)
+	     (or (nth 3 state)
+		 (nth 4 state)
+		 (looking-at (concat "[ \t]*" comment-start-skip))
+		 (save-excursion
+		   (skip-chars-backward " \t")
+		   (goto-char (- (point) 2))
+		   (looking-at "\\*/"))))
 	(forward-sentence (- count))
       (while (> count 0)
 	(c-beginning-of-statement-1)
@@ -2973,7 +2974,7 @@ region."
 
 ;; defuns for submitting bug reports
 
-(defconst c-version "$Revision: 3.188 $"
+(defconst c-version "$Revision: 3.189 $"
   "cc-mode version number.")
 (defconst c-mode-help-address "cc-mode-help@anthem.nlm.nih.gov"
   "Address accepting submission of bug reports.")
