b8c761ee9001d462563a1f3cdcf3dab8252628f5
warsaw <devnull@localhost>
1994-01-13 23:25:57 +0000

    (c-guess-basic-semantics): CASE 8A substatement. relpos is boi of placeholder

--- cc-mode.el
+++ cc-mode.el
@@ -5,8 +5,8 @@
 ;;          1985 Richard M. Stallman
 ;; Maintainer: cc-mode-help@anthem.nlm.nih.gov
 ;; Created: a long, long, time ago. adapted from the original c-mode.el
-;; Version:         $Revision: 3.192 $
-;; Last Modified:   $Date: 1994-01-13 04:24:55 $
+;; Version:         $Revision: 3.193 $
+;; Last Modified:   $Date: 1994-01-13 23:25:57 $
 ;; Keywords: C++ C editing major-mode
 
 ;; Copyright (C) 1992, 1993, 1994 Barry A. Warsaw
@@ -85,7 +85,7 @@
 ;; LCD Archive Entry:
 ;; cc-mode.el|Barry A. Warsaw|cc-mode-help@anthem.nlm.nih.gov
 ;; |Major mode for editing C++, and ANSI/K&R C code
-;; |$Date: 1994-01-13 04:24:55 $|$Revision: 3.192 $|
+;; |$Date: 1994-01-13 23:25:57 $|$Revision: 3.193 $|
 
 ;;; Code:
 
@@ -685,7 +685,7 @@ behavior that users are familiar with.")
 ;;;###autoload
 (defun c++-mode ()
   "Major mode for editing C++ code.
-cc-mode Revision: $Revision: 3.192 $
+cc-mode Revision: $Revision: 3.193 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c++-mode buffer.  This automatically sets up a mail buffer with
 version information already added.  You just need to add a description
@@ -716,7 +716,7 @@ Key bindings:
 ;;;###autoload
 (defun c-mode ()
   "Major mode for editing K&R and ANSI C code.
-cc-mode Revision: $Revision: 3.192 $
+cc-mode Revision: $Revision: 3.193 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c-mode buffer.  This automatically sets up a mail buffer with version
 information already added.  You just need to add a description of the
@@ -2576,7 +2576,8 @@ Optional SHUTUP-P if non-nil, inhibits message printing and error checking."
 				  t))
 		   (progn (c-forward-syntactic-ws)
 			  (>= (point) indent-point))))
-	    (c-add-semantics 'substatement placeholder)
+	    (goto-char placeholder)
+	    (c-add-semantics 'substatement (c-point 'boi))
 	    (if (= char-after-ip ?{)
 		(c-add-semantics 'block-open)))
 	   ;; CASE 8B: open braces for class or brace-lists
@@ -2978,7 +2979,7 @@ region."
 
 ;; defuns for submitting bug reports
 
-(defconst c-version "$Revision: 3.192 $"
+(defconst c-version "$Revision: 3.193 $"
   "cc-mode version number.")
 (defconst c-mode-help-address "cc-mode-help@anthem.nlm.nih.gov"
   "Address accepting submission of bug reports.")
