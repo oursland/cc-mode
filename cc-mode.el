16006c184bc69cc20a10da6b19a3fcaf0fd92afe
warsaw <devnull@localhost>
1994-02-08 21:57:19 +0000

    (c-guess-basic-semantics): when narrowing out enclosing class, and calculating start of region to narrow to, do not skip all syntactic ws, just lexical whitespace (change c-forward-syntactic-ws call to skip-chars-forward). Otherwise, you could skip into the middle of a C comment block and not figure out that what you're looking at is a `c' syntactic symbol.

--- cc-mode.el
+++ cc-mode.el
@@ -5,8 +5,8 @@
 ;;          1985 Richard M. Stallman
 ;; Maintainer: cc-mode-help@anthem.nlm.nih.gov
 ;; Created: a long, long, time ago. adapted from the original c-mode.el
-;; Version:         $Revision: 3.234 $
-;; Last Modified:   $Date: 1994-02-08 21:28:52 $
+;; Version:         $Revision: 3.235 $
+;; Last Modified:   $Date: 1994-02-08 21:57:19 $
 ;; Keywords: C++ C editing major-mode
 
 ;; Copyright (C) 1992, 1993, 1994 Barry A. Warsaw
@@ -92,7 +92,7 @@
 ;; LCD Archive Entry:
 ;; cc-mode.el|Barry A. Warsaw|cc-mode-help@anthem.nlm.nih.gov
 ;; |Major mode for editing C++, and ANSI/K&R C code
-;; |$Date: 1994-02-08 21:28:52 $|$Revision: 3.234 $|
+;; |$Date: 1994-02-08 21:57:19 $|$Revision: 3.235 $|
 
 ;;; Code:
 
@@ -702,7 +702,7 @@ behavior that users are familiar with.")
 ;;;###autoload
 (defun c++-mode ()
   "Major mode for editing C++ code.
-cc-mode Revision: $Revision: 3.234 $
+cc-mode Revision: $Revision: 3.235 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c++-mode buffer.  This automatically sets up a mail buffer with
 version information already added.  You just need to add a description
@@ -733,7 +733,7 @@ Key bindings:
 ;;;###autoload
 (defun c-mode ()
   "Major mode for editing K&R and ANSI C code.
-cc-mode Revision: $Revision: 3.234 $
+cc-mode Revision: $Revision: 3.235 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c-mode buffer.  This automatically sets up a mail buffer with version
 information already added.  You just need to add a description of the
@@ -2272,7 +2272,7 @@ Optional SHUTUP-P if non-nil, inhibits message printing and error checking."
 	      (narrow-to-region
 	       (progn
 		 (goto-char (1+ (car inclass-p)))
-		 (c-forward-syntactic-ws indent-point)
+		 (skip-chars-forward " \t" indent-point)
 		 (c-point 'bol))
 	       (progn
 		 (goto-char indent-point)
@@ -3087,7 +3087,7 @@ it trailing backslashes are removed."
 
 ;; defuns for submitting bug reports
 
-(defconst c-version "$Revision: 3.234 $"
+(defconst c-version "$Revision: 3.235 $"
   "cc-mode version number.")
 (defconst c-mode-help-address "cc-mode-help@anthem.nlm.nih.gov"
   "Address accepting submission of bug reports.")
