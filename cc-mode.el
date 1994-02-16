c4f1cdb18dd5fe9136dd0107e8a6e4221b44c2e0
warsaw <devnull@localhost>
1994-02-16 23:06:15 +0000

    (c++-mode-syntax-table): change : back to default (. == punctuation) syntax class since c-label-key is busted when its (_ == symbol)

--- cc-mode.el
+++ cc-mode.el
@@ -5,8 +5,8 @@
 ;;          1985 Richard M. Stallman
 ;; Maintainer: cc-mode-help@anthem.nlm.nih.gov
 ;; Created: a long, long, time ago. adapted from the original c-mode.el
-;; Version:         $Revision: 3.249 $
-;; Last Modified:   $Date: 1994-02-16 20:35:05 $
+;; Version:         $Revision: 3.250 $
+;; Last Modified:   $Date: 1994-02-16 23:06:15 $
 ;; Keywords: C++ C editing major-mode
 
 ;; Copyright (C) 1992, 1993, 1994 Barry A. Warsaw
@@ -93,7 +93,7 @@
 ;; LCD Archive Entry:
 ;; cc-mode.el|Barry A. Warsaw|cc-mode-help@anthem.nlm.nih.gov
 ;; |Major mode for editing C++, and ANSI/K&R C code
-;; |$Date: 1994-02-16 20:35:05 $|$Revision: 3.249 $|
+;; |$Date: 1994-02-16 23:06:15 $|$Revision: 3.250 $|
 
 ;;; Code:
 
@@ -648,8 +648,14 @@ supported list, along with the values for this variable:
     (modify-syntax-entry ?*  ". 23"   c++-mode-syntax-table)
     (modify-syntax-entry ?\n "> b"    c++-mode-syntax-table))
    )
-  ;; TBD: does it make sense for colon to be symbol class in C++?  I think, yes.
-  (modify-syntax-entry ?: "_" c++-mode-syntax-table))
+  ;; TBD: does it make sense for colon to be symbol class in C++?
+  ;; I'm not so sure, since c-label-key is busted on lines like:
+  ;; Foo::bar( i );
+  ;; maybe c-label-key should be fixed instead of commenting this out,
+  ;; but it also bothers me that this only seems appropriate for C++
+  ;; and not C.
+  ;;(modify-syntax-entry ?: "_" c++-mode-syntax-table)
+  )
 
 (defvar c-hungry-delete-key nil
   "Internal state of hungry delete key feature.")
@@ -715,7 +721,7 @@ behavior that users are familiar with.")
 ;;;###autoload
 (defun c++-mode ()
   "Major mode for editing C++ code.
-cc-mode Revision: $Revision: 3.249 $
+cc-mode Revision: $Revision: 3.250 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c++-mode buffer.  This automatically sets up a mail buffer with
 version information already added.  You just need to add a description
@@ -746,7 +752,7 @@ Key bindings:
 ;;;###autoload
 (defun c-mode ()
   "Major mode for editing K&R and ANSI C code.
-cc-mode Revision: $Revision: 3.249 $
+cc-mode Revision: $Revision: 3.250 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c-mode buffer.  This automatically sets up a mail buffer with version
 information already added.  You just need to add a description of the
@@ -3142,7 +3148,7 @@ it trailing backslashes are removed."
 
 ;; defuns for submitting bug reports
 
-(defconst c-version "$Revision: 3.249 $"
+(defconst c-version "$Revision: 3.250 $"
   "cc-mode version number.")
 (defconst c-mode-help-address "cc-mode-help@anthem.nlm.nih.gov"
   "Address accepting submission of bug reports.")
