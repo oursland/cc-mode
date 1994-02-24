29f71884d4d40fe54b41da9faeeb479a29d3992d
warsaw <devnull@localhost>
1994-02-24 23:09:43 +0000

    (c-set-offset): changed the minibuffer prompt.  "Langelem" => "Syntactic symbol", and put in a brief message so that people won't quote the symbol (as some novices have been doing).

--- cc-mode.el
+++ cc-mode.el
@@ -5,8 +5,8 @@
 ;;          1985 Richard M. Stallman
 ;; Maintainer: cc-mode-help@anthem.nlm.nih.gov
 ;; Created: a long, long, time ago. adapted from the original c-mode.el
-;; Version:         $Revision: 3.266 $
-;; Last Modified:   $Date: 1994-02-24 21:51:24 $
+;; Version:         $Revision: 3.267 $
+;; Last Modified:   $Date: 1994-02-24 23:09:43 $
 ;; Keywords: C++ C editing major-mode
 
 ;; Copyright (C) 1992, 1993, 1994 Barry A. Warsaw
@@ -93,7 +93,7 @@
 ;; LCD Archive Entry:
 ;; cc-mode.el|Barry A. Warsaw|cc-mode-help@anthem.nlm.nih.gov
 ;; |Major mode for editing C++, and ANSI/K&R C code
-;; |$Date: 1994-02-24 21:51:24 $|$Revision: 3.266 $|
+;; |$Date: 1994-02-24 23:09:43 $|$Revision: 3.267 $|
 
 ;;; Code:
 
@@ -770,7 +770,7 @@ behavior that users are familiar with.")
 ;;;###autoload
 (defun c++-mode ()
   "Major mode for editing C++ code.
-cc-mode Revision: $Revision: 3.266 $
+cc-mode Revision: $Revision: 3.267 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c++-mode buffer.  This automatically sets up a mail buffer with
 version information already added.  You just need to add a description
@@ -801,7 +801,7 @@ Key bindings:
 ;;;###autoload
 (defun c-mode ()
   "Major mode for editing K&R and ANSI C code.
-cc-mode Revision: $Revision: 3.266 $
+cc-mode Revision: $Revision: 3.267 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c-mode buffer.  This automatically sets up a mail buffer with version
 information already added.  You just need to add a description of the
@@ -1388,9 +1388,9 @@ offset for that syntactic element.  Optional ADD says to add SYMBOL to
   (interactive
    (let* ((langelem
 	   (intern (completing-read
-		    (concat "Langelem symbol to change"
+		    (concat "Syntactic symbol to change"
 			    (if current-prefix-arg " or add" "")
-			    ": ")
+			    " (do not quote): ")
 		    (mapcar
 		     (function
 		      (lambda (langelem)
@@ -3224,7 +3224,7 @@ it trailing backslashes are removed."
 
 ;; defuns for submitting bug reports
 
-(defconst c-version "$Revision: 3.266 $"
+(defconst c-version "$Revision: 3.267 $"
   "cc-mode version number.")
 (defconst c-mode-help-address "cc-mode-help@anthem.nlm.nih.gov"
   "Address accepting submission of bug reports.")
