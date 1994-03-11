2c578dcd31e26560f5a496dc963d2890b0d2965f
warsaw <devnull@localhost>
1994-03-11 17:43:09 +0000

    (c-guess-basic-semantics): CASE 8C: after moving to placeholder, which is bos, move over any conditional statments if looking at c-conditional-key.  Otherwise the following code indents incorrectly:
    main()
    {
        while (cin >> float)
            cout << float
                 << float;   //  This should not be this far over but beneath <<
    }

--- cc-mode.el
+++ cc-mode.el
@@ -5,8 +5,8 @@
 ;;          1985 Richard M. Stallman
 ;; Maintainer: cc-mode-help@anthem.nlm.nih.gov
 ;; Created: a long, long, time ago. adapted from the original c-mode.el
-;; Version:         $Revision: 3.283 $
-;; Last Modified:   $Date: 1994-03-11 17:14:31 $
+;; Version:         $Revision: 3.284 $
+;; Last Modified:   $Date: 1994-03-11 17:43:09 $
 ;; Keywords: C++ C editing major-mode
 
 ;; Copyright (C) 1992, 1993, 1994 Barry A. Warsaw
@@ -93,7 +93,7 @@
 ;; LCD Archive Entry:
 ;; cc-mode.el|Barry A. Warsaw|cc-mode-help@anthem.nlm.nih.gov
 ;; |Major mode for editing C++, and ANSI/K&R C code
-;; |$Date: 1994-03-11 17:14:31 $|$Revision: 3.283 $|
+;; |$Date: 1994-03-11 17:43:09 $|$Revision: 3.284 $|
 
 ;;; Code:
 
@@ -783,7 +783,7 @@ behavior that users are familiar with.")
 ;;;###autoload
 (defun c++-mode ()
   "Major mode for editing C++ code.
-cc-mode Revision: $Revision: 3.283 $
+cc-mode Revision: $Revision: 3.284 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c++-mode buffer.  This automatically sets up a mail buffer with
 version information already added.  You just need to add a description
@@ -814,7 +814,7 @@ Key bindings:
 ;;;###autoload
 (defun c-mode ()
   "Major mode for editing K&R and ANSI C code.
-cc-mode Revision: $Revision: 3.283 $
+cc-mode Revision: $Revision: 3.284 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c-mode buffer.  This automatically sets up a mail buffer with version
 information already added.  You just need to add a description of the
@@ -2784,6 +2784,8 @@ Optional SHUTUP-P if non-nil, inhibits message printing and error checking."
 	   ;; CASE 8C: iostream insertion or extraction operator
 	   ((looking-at "<<\\|>>")
 	    (goto-char placeholder)
+	    (if (looking-at c-conditional-key)
+		(c-skip-conditional))
 	    (while (and (re-search-forward "<<\\|>>" indent-point 'move)
 			(c-in-literal)))
 	    ;; if we ended up at indent-point, then the first streamop
@@ -3276,7 +3278,7 @@ it trailing backslashes are removed."
 
 ;; defuns for submitting bug reports
 
-(defconst c-version "$Revision: 3.283 $"
+(defconst c-version "$Revision: 3.284 $"
   "cc-mode version number.")
 (defconst c-mode-help-address "cc-mode-help@anthem.nlm.nih.gov"
   "Address accepting submission of bug reports.")
