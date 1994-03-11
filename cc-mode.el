f9786f74e360b2f4bd252f31bdbffce90e68b7bd
warsaw <devnull@localhost>
1994-03-11 03:37:02 +0000

    #(c-indent-region): minor docstring indentation

--- cc-mode.el
+++ cc-mode.el
@@ -5,8 +5,8 @@
 ;;          1985 Richard M. Stallman
 ;; Maintainer: cc-mode-help@anthem.nlm.nih.gov
 ;; Created: a long, long, time ago. adapted from the original c-mode.el
-;; Version:         $Revision: 3.279 $
-;; Last Modified:   $Date: 1994-03-11 03:33:25 $
+;; Version:         $Revision: 3.280 $
+;; Last Modified:   $Date: 1994-03-11 03:37:02 $
 ;; Keywords: C++ C editing major-mode
 
 ;; Copyright (C) 1992, 1993, 1994 Barry A. Warsaw
@@ -93,7 +93,7 @@
 ;; LCD Archive Entry:
 ;; cc-mode.el|Barry A. Warsaw|cc-mode-help@anthem.nlm.nih.gov
 ;; |Major mode for editing C++, and ANSI/K&R C code
-;; |$Date: 1994-03-11 03:33:25 $|$Revision: 3.279 $|
+;; |$Date: 1994-03-11 03:37:02 $|$Revision: 3.280 $|
 
 ;;; Code:
 
@@ -783,7 +783,7 @@ behavior that users are familiar with.")
 ;;;###autoload
 (defun c++-mode ()
   "Major mode for editing C++ code.
-cc-mode Revision: $Revision: 3.279 $
+cc-mode Revision: $Revision: 3.280 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c++-mode buffer.  This automatically sets up a mail buffer with
 version information already added.  You just need to add a description
@@ -814,7 +814,7 @@ Key bindings:
 ;;;###autoload
 (defun c-mode ()
   "Major mode for editing K&R and ANSI C code.
-cc-mode Revision: $Revision: 3.279 $
+cc-mode Revision: $Revision: 3.280 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c-mode buffer.  This automatically sets up a mail buffer with version
 information already added.  You just need to add a description of the
@@ -2034,7 +2034,8 @@ Optional SHUTUP-P if non-nil, inhibits message printing and error checking."
 	    (save-excursion
 	      ;; Find beginning of following line.
 	      (setq nextline (c-point 'bonl))
-	      ;; Find first beginning-of-sexp for sexp extending past this line.
+	      ;; Find first beginning-of-sexp for sexp extending past
+	      ;; this line.
 	      (beginning-of-line)
 	      (while (< (point) nextline)
 		(condition-case nil
@@ -3266,7 +3267,7 @@ it trailing backslashes are removed."
 
 ;; defuns for submitting bug reports
 
-(defconst c-version "$Revision: 3.279 $"
+(defconst c-version "$Revision: 3.280 $"
   "cc-mode version number.")
 (defconst c-mode-help-address "cc-mode-help@anthem.nlm.nih.gov"
   "Address accepting submission of bug reports.")
