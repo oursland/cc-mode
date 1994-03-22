9a6bd13e5b0f1e78107a53a04de8cbd024cb50c8
warsaw <devnull@localhost>
1994-03-22 18:25:12 +0000

    (c-inside-bracelist-p): in the first goto-char, goto containing-sexp, not 1- c-s, otherwise, it will not properly recognize bracelists with comments on the end of the line.  The following c-beginning-of-statement will properly consume any preceding whitespace.

--- cc-mode.el
+++ cc-mode.el
@@ -5,8 +5,8 @@
 ;;          1985 Richard M. Stallman
 ;; Maintainer: cc-mode-help@anthem.nlm.nih.gov
 ;; Created: a long, long, time ago. adapted from the original c-mode.el
-;; Version:         $Revision: 3.295 $
-;; Last Modified:   $Date: 1994-03-21 22:01:25 $
+;; Version:         $Revision: 3.296 $
+;; Last Modified:   $Date: 1994-03-22 18:25:12 $
 ;; Keywords: C++ C editing major-mode
 
 ;; Copyright (C) 1992, 1993, 1994 Barry A. Warsaw
@@ -93,7 +93,7 @@
 ;; LCD Archive Entry:
 ;; cc-mode.el|Barry A. Warsaw|cc-mode-help@anthem.nlm.nih.gov
 ;; |Major mode for editing C++, and ANSI/K&R C code
-;; |$Date: 1994-03-21 22:01:25 $|$Revision: 3.295 $|
+;; |$Date: 1994-03-22 18:25:12 $|$Revision: 3.296 $|
 
 ;;; Code:
 
@@ -786,7 +786,7 @@ behavior that users are familiar with.")
 ;;;###autoload
 (defun c++-mode ()
   "Major mode for editing C++ code.
-cc-mode Revision: $Revision: 3.295 $
+cc-mode Revision: $Revision: 3.296 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c++-mode buffer.  This automatically sets up a mail buffer with
 version information already added.  You just need to add a description
@@ -817,7 +817,7 @@ Key bindings:
 ;;;###autoload
 (defun c-mode ()
   "Major mode for editing K&R and ANSI C code.
-cc-mode Revision: $Revision: 3.295 $
+cc-mode Revision: $Revision: 3.296 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c-mode buffer.  This automatically sets up a mail buffer with version
 information already added.  You just need to add a description of the
@@ -2378,7 +2378,7 @@ Optional SHUTUP-P if non-nil, inhibits message printing and error checking."
     (save-excursion
       (or
        ;; this will pick up enum lists
-       (progn (goto-char (1- containing-sexp))
+       (progn (goto-char containing-sexp)
 	      (c-beginning-of-statement)
 	      (setq bufpos (point))
 	      (and (< bufpos containing-sexp)
@@ -3304,7 +3304,7 @@ it trailing backslashes are removed."
 
 ;; defuns for submitting bug reports
 
-(defconst c-version "$Revision: 3.295 $"
+(defconst c-version "$Revision: 3.296 $"
   "cc-mode version number.")
 (defconst c-mode-help-address "cc-mode-help@anthem.nlm.nih.gov"
   "Address accepting submission of bug reports.")
