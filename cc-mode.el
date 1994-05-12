d9608d8a12304107e27a338f419d298b6f0d2e62
warsaw <devnull@localhost>
1994-05-12 19:32:42 +0000

    #(c-guess-basic-semantics): CASE 4G; a better patch than last time

--- cc-mode.el
+++ cc-mode.el
@@ -5,8 +5,8 @@
 ;;          1985 Richard M. Stallman
 ;; Maintainer: cc-mode-help@anthem.nlm.nih.gov
 ;; Created: a long, long, time ago. adapted from the original c-mode.el
-;; Version:         $Revision: 3.331 $
-;; Last Modified:   $Date: 1994-05-12 19:30:10 $
+;; Version:         $Revision: 3.332 $
+;; Last Modified:   $Date: 1994-05-12 19:32:42 $
 ;; Keywords: C++ C editing major-mode
 
 ;; Copyright (C) 1992, 1993, 1994 Barry A. Warsaw
@@ -93,7 +93,7 @@
 ;; LCD Archive Entry:
 ;; cc-mode.el|Barry A. Warsaw|cc-mode-help@anthem.nlm.nih.gov
 ;; |Major mode for editing C++, and ANSI/K&R C code
-;; |$Date: 1994-05-12 19:30:10 $|$Revision: 3.331 $|
+;; |$Date: 1994-05-12 19:32:42 $|$Revision: 3.332 $|
 
 ;;; Code:
 
@@ -802,7 +802,7 @@ behavior that users are familiar with.")
 ;;;###autoload
 (defun c++-mode ()
   "Major mode for editing C++ code.
-cc-mode Revision: $Revision: 3.331 $
+cc-mode Revision: $Revision: 3.332 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c++-mode buffer.  This automatically sets up a mail buffer with
 version information already added.  You just need to add a description
@@ -834,7 +834,7 @@ Key bindings:
 ;;;###autoload
 (defun c-mode ()
   "Major mode for editing K&R and ANSI C code.
-cc-mode Revision: $Revision: 3.331 $
+cc-mode Revision: $Revision: 3.332 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c-mode buffer.  This automatically sets up a mail buffer with version
 information already added.  You just need to add a description of the
@@ -2804,7 +2804,7 @@ Optional SHUTUP-P if non-nil, inhibits message printing and error checking."
 		   (= (preceding-char) ?\)))
 		 (save-excursion
 		   (c-beginning-of-statement)
-		   (not (looking-at "typedef"))))
+		   (not (looking-at "typedef[ \t\n]+"))))
 	    (goto-char placeholder)
 	    (c-add-semantics 'knr-argdecl (c-point 'boi)))
 	   ;; CASE 4H: we are at the topmost level, make sure we skip
@@ -3485,7 +3485,7 @@ it trailing backslashes are removed."
 
 ;; defuns for submitting bug reports
 
-(defconst c-version "$Revision: 3.331 $"
+(defconst c-version "$Revision: 3.332 $"
   "cc-mode version number.")
 (defconst c-mode-help-address "cc-mode-help@anthem.nlm.nih.gov"
   "Address accepting submission of bug reports.")
