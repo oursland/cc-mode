01d246e02076dce5542160a956af4d17eb8c9639
warsaw <devnull@localhost>
1994-05-31 23:04:09 +0000

    (c-guess-basic-semantics): CASE 14.E, if defun opening brace is not at boi, its because its hung on right side so we need a different relpos

--- cc-mode.el
+++ cc-mode.el
@@ -5,8 +5,8 @@
 ;;          1985 Richard M. Stallman
 ;; Maintainer: cc-mode-help@anthem.nlm.nih.gov
 ;; Created: a long, long, time ago. adapted from the original c-mode.el
-;; Version:         $Revision: 3.352 $
-;; Last Modified:   $Date: 1994-05-31 22:21:35 $
+;; Version:         $Revision: 3.353 $
+;; Last Modified:   $Date: 1994-05-31 23:04:09 $
 ;; Keywords: C++ C editing major-mode
 
 ;; Copyright (C) 1992, 1993, 1994 Barry A. Warsaw
@@ -93,7 +93,7 @@
 ;; LCD Archive Entry:
 ;; cc-mode.el|Barry A. Warsaw|cc-mode-help@anthem.nlm.nih.gov
 ;; |Major mode for editing C++, and ANSI/K&R C code
-;; |$Date: 1994-05-31 22:21:35 $|$Revision: 3.352 $|
+;; |$Date: 1994-05-31 23:04:09 $|$Revision: 3.353 $|
 
 ;;; Code:
 
@@ -834,7 +834,7 @@ behavior that users are familiar with.")
 ;;;###autoload
 (defun c++-mode ()
   "Major mode for editing C++ code.
-cc-mode Revision: $Revision: 3.352 $
+cc-mode Revision: $Revision: 3.353 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c++-mode buffer.  This automatically sets up a mail buffer with
 version information already added.  You just need to add a description
@@ -867,7 +867,7 @@ Key bindings:
 ;;;###autoload
 (defun c-mode ()
   "Major mode for editing K&R and ANSI C code.
-cc-mode Revision: $Revision: 3.352 $
+cc-mode Revision: $Revision: 3.353 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c-mode buffer.  This automatically sets up a mail buffer with version
 information already added.  You just need to add a description of the
@@ -3236,6 +3236,14 @@ Optional SHUTUP-P if non-nil, inhibits message printing and error checking."
 		  (c-narrow-out-enclosing-class state containing-sexp)
 		  (not (c-enclosing-brace state))))
 	      (goto-char containing-sexp)
+	      ;; if not at boi, then defun-opening braces are hung on
+	      ;; right side, so we need a different relpos
+	      (if (/= (point) (c-point 'boi))
+		  (progn
+		    (c-backward-syntactic-ws)
+		    (c-safe (forward-sexp (if (= (preceding-char) ?\))
+					      -1 -2)))
+		    ))
 	      (c-add-semantics 'defun-block-intro (c-point 'boi)))
 	     ;; CASE 14.F: first statement in a block
 	     (t (goto-char containing-sexp)
@@ -3608,7 +3616,7 @@ it trailing backslashes are removed."
 
 ;; defuns for submitting bug reports
 
-(defconst c-version "$Revision: 3.352 $"
+(defconst c-version "$Revision: 3.353 $"
   "cc-mode version number.")
 (defconst c-mode-help-address "cc-mode-help@anthem.nlm.nih.gov"
   "Address accepting submission of bug reports.")
