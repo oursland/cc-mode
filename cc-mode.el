10a2f79297f1d8aa6b288e6a54f7bee2ffe2d906
warsaw <devnull@localhost>
1994-07-12 14:05:29 +0000

    (c-parse-state): small but important fix in `something bad happened' test. make sure that placeholder close brace ends up before indent point, otherwise we didn't cross it.

--- cc-mode.el
+++ cc-mode.el
@@ -5,8 +5,8 @@
 ;;          1985 Richard M. Stallman
 ;; Maintainer: cc-mode-help@anthem.nlm.nih.gov
 ;; Created: a long, long, time ago. adapted from the original c-mode.el
-;; Version:         $Revision: 4.30 $
-;; Last Modified:   $Date: 1994-07-07 13:56:08 $
+;; Version:         $Revision: 4.31 $
+;; Last Modified:   $Date: 1994-07-12 14:05:29 $
 ;; Keywords: C++ C Objective-C editing major-mode
 
 ;; Copyright (C) 1992, 1993, 1994 Barry A. Warsaw
@@ -93,7 +93,7 @@
 ;; LCD Archive Entry:
 ;; cc-mode.el|Barry A. Warsaw|cc-mode-help@anthem.nlm.nih.gov
 ;; |Major mode for editing C++, and ANSI/K&R C code
-;; |$Date: 1994-07-07 13:56:08 $|$Revision: 4.30 $|
+;; |$Date: 1994-07-12 14:05:29 $|$Revision: 4.31 $|
 
 ;;; Code:
 
@@ -898,7 +898,7 @@ behavior that users are familiar with.")
 ;;;###autoload
 (defun c++-mode ()
   "Major mode for editing C++ code.
-cc-mode Revision: $Revision: 4.30 $
+cc-mode Revision: $Revision: 4.31 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c++-mode buffer.  This automatically sets up a mail buffer with
 version information already added.  You just need to add a description
@@ -931,7 +931,7 @@ Key bindings:
 ;;;###autoload
 (defun c-mode ()
   "Major mode for editing K&R and ANSI C code.
-cc-mode Revision: $Revision: 4.30 $
+cc-mode Revision: $Revision: 4.31 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c-mode buffer.  This automatically sets up a mail buffer with version
 information already added.  You just need to add a description of the
@@ -964,7 +964,7 @@ Key bindings:
 ;;;###autoload
 (defun objc-mode ()
   "Major mode for editing Objective C code.
-cc-mode Revision: $Revision: 4.30 $
+cc-mode Revision: $Revision: 4.31 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from an
 objc-mode buffer.  This automatically sets up a mail buffer with
 version information already added.  You just need to add a description
@@ -2494,6 +2494,7 @@ Optional SHUTUP-P if non-nil, inhibits message printing and error checking."
 		 (if (and (not pos) (not at-bob)
 			  (setq placeholder (c-safe (scan-lists last-pos 1 1)))
 			  ;;(char-after (1- placeholder))
+			  (<= placeholder here)
 			  (= (char-after (1- placeholder)) ?\}))
 		     (save-excursion
 		       (let (donep)
@@ -3843,7 +3844,7 @@ it trailing backslashes are removed."
 
 ;; defuns for submitting bug reports
 
-(defconst c-version "$Revision: 4.30 $"
+(defconst c-version "$Revision: 4.31 $"
   "cc-mode version number.")
 (defconst c-mode-help-address "cc-mode-help@anthem.nlm.nih.gov"
   "Address accepting submission of bug reports.")
