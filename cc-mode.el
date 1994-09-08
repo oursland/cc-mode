07ce01d21f8b0b4cde73ee04b1831d9133699990
warsaw <devnull@localhost>
1994-09-08 14:19:15 +0000

    #(c-indent-region): removed unnecessary laststart variable

--- cc-mode.el
+++ cc-mode.el
@@ -5,8 +5,8 @@
 ;;          1985 Richard M. Stallman
 ;; Maintainer: cc-mode-help@anthem.nlm.nih.gov
 ;; Created: a long, long, time ago. adapted from the original c-mode.el
-;; Version:         $Revision: 4.83 $
-;; Last Modified:   $Date: 1994-09-07 18:41:29 $
+;; Version:         $Revision: 4.84 $
+;; Last Modified:   $Date: 1994-09-08 14:19:15 $
 ;; Keywords: C++ C Objective-C editing major-mode
 
 ;; Copyright (C) 1992, 1993, 1994 Barry A. Warsaw
@@ -99,7 +99,7 @@
 ;; LCD Archive Entry:
 ;; cc-mode.el|Barry A. Warsaw|cc-mode-help@anthem.nlm.nih.gov
 ;; |Major mode for editing C++, Objective-C, and ANSI/K&R C code
-;; |$Date: 1994-09-07 18:41:29 $|$Revision: 4.83 $|
+;; |$Date: 1994-09-08 14:19:15 $|$Revision: 4.84 $|
 
 ;;; Code:
 
@@ -967,7 +967,7 @@ behavior that users are familiar with.")
 ;;;###autoload
 (defun c++-mode ()
   "Major mode for editing C++ code.
-cc-mode Revision: $Revision: 4.83 $
+cc-mode Revision: $Revision: 4.84 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c++-mode buffer.  This automatically sets up a mail buffer with
 version information already added.  You just need to add a description
@@ -1006,7 +1006,7 @@ Key bindings:
 ;;;###autoload
 (defun c-mode ()
   "Major mode for editing K&R and ANSI C code.
-cc-mode Revision: $Revision: 4.83 $
+cc-mode Revision: $Revision: 4.84 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from a
 c-mode buffer.  This automatically sets up a mail buffer with version
 information already added.  You just need to add a description of the
@@ -1043,7 +1043,7 @@ Key bindings:
 ;;;###autoload
 (defun objc-mode ()
   "Major mode for editing Objective C code.
-cc-mode Revision: $Revision: 4.83 $
+cc-mode Revision: $Revision: 4.84 $
 To submit a problem report, enter `\\[c-submit-bug-report]' from an
 objc-mode buffer.  This automatically sets up a mail buffer with
 version information already added.  You just need to add a description
@@ -2419,7 +2419,6 @@ Optional SHUTUP-P if non-nil, inhibits message printing and error checking."
 			(< (point) endmark))
 	      ;; update progress
 	      (c-progress-update)
-	      (setq laststart (point))
 	      ;; Indent one line as with TAB.
 	      (let (nextline sexpend sexpbeg)
 		;; skip blank lines
@@ -4115,7 +4114,7 @@ it trailing backslashes are removed."
 
 ;; defuns for submitting bug reports
 
-(defconst c-version "$Revision: 4.83 $"
+(defconst c-version "$Revision: 4.84 $"
   "cc-mode version number.")
 (defconst c-mode-help-address "cc-mode-help@anthem.nlm.nih.gov"
   "Address accepting submission of bug reports.")
