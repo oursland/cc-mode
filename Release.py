96eb0f9c5e32319e107d251f0fc12be27c4285ef
warsaw <devnull@localhost>
1994-09-10 18:38:47 +0000

    new hacked up release script

--- Release.py
+++ Release.py
@@ -1,129 +1,61 @@
-#! /bin/perl
+#! /depot/gnu/plat/bin/perl
 #
-# Perl script to support version release of Supercite
-# $Id: Release.py,v 1.2 1993-09-30 01:50:28 warsaw Exp $
+# Perl script to support public releases of cc-mode
+# $Id: Release.py,v 1.3 1994-09-10 18:38:47 warsaw Exp $
 #
-# Usage:
-# -p  -- non-beta release (i.e. public)
-# -n  -- add symbolic name of release to head of branch
-# -N  -- force symbolic of release to head of branch
-# -d  -- create the RCS diff (i.e. patch) file
-# -d- -- overwrite the generated patch file if one already exists
-# -t  -- generate a new tar file
-# -t- -- overwrite new tar file if one already exists
-# -h  -- help message
 
-# Variables, default to beta release
-@Lisp = ('c++-mode');
-@Files = (grep(($_ .= ".el") && 1, @Lisp));
-$Release = "Beta";
+# constants
+@Lisp = ("cc-compat", "cc-mode", "cc-mode-18");
+@Text = ("ChangeLog", "DUMPING", "MANIFEST", "README");
+@Files = (grep(($_ .= ".el") && 1, @Lisp), @Text);
+$Main = "cc-mode.el";
+$Tarfile = "cc-mode-XXX.tar";
+$Release = "X--------------------->Full";
+$Compress = "gzip";
 
-# Usage
-sub usage {
-	print "Usage: $0: [-p] [-n] [-N] [-d[-]] [-t[-]]\n";
-	print "-p  -- non Beta (i.e. Public release\n";
-	print "-n  -- add symbolic name of release to head of branch\n";
-	print "-N  -- force symbolic of release to head of branch\n";
-	print "-d  -- create the RCS diff (i.e. patch) file\n";
-	print "-d- -- overwrite the generated patch file if one already exists\n";
-	print "-t  -- generate new tar file\n";
-	print "-t- -- overwrite new tar file if one already exists\n";
-	print "-h  -- help message\n";
-}
-
-
-# scan argv
-while ($_ = $ARGV[0], /^-/) {
-	shift;
-	/^-p$/  && ($Release = "Public", next);
-	/^-r$/  && (++$Release, next);
-	/^-n$/  && (++$AddSymName, next);
-	/^-N$/  && (++$AddSymName, ++$ForceSymName, next);
-	/^-d-$/ && (++$OverwriteDiff, ++$GenerateDiff, next);
-	/^-d$/  && (++$GenerateDiff, next);
-	/^-t-$/ && (++$GenerateTar, ++$OverwriteTar, next);
-	/^-t$/  && (++$GenerateTar, next);
-	/^-h(elp)?/ && (++$Help, next);
-
-	# bad switch
-	print "$0: illegal switch $_\n";
-	++$ArgvError;
-}
-
-&usage() if $ArgvError || $Help;
-exit(0) if $Help;
-exit(1) if $ArgvError;
 
 # Calculate the new version number
+$_ = (grep(/^head:/, `rlog -h $Main`))[0];
+die "Could not extract branch head from $Main.\n"
+    if ( ! $_ || ! /^head:\s*([0-9.]+)/);
+
+$Revision = $1;
+$Tarfile =~ s/XXX/$Revision/;
+print "Using revision number $Revision and tarfile: $Tarfile\n";
+
+
+# Make sure files we're going to tar up are not RCS locked
+@Locks = `rlog -L -R -l @Files`;
+die "The following distribution files are still locked: @Locks\n"
+    if @Locks;
+
+
+# set the symbol name for the branch heads of all the files.
 ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+
 $Month = ("Jan", "Feb", "Mar", "Apr", "May", "Jun",
-		  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")[$mon];
-$Date = "${mday}_${Month}_${year}";
-$NewVersion = "${Release}_${Date}";
-print "New Supercite version: $NewVersion\n";
+	  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")[$mon];
 
-# Now do the RCS massaging on each file
-if ($GenerateDiff) {
-	$DiffFile = "sc-diff-$MajorRev.$MinorRev-$MajorRev.$NewMinorRev";
-	if (-e $DiffFile) {
-		die "Diff file exists: $DiffFile\n" if !$OverwriteDiff;
-		print "Overwriting existing Diff file: $DiffFile\n";
-	}
-	else {
-		print "Diff'ing to file: $DiffFile\n";
-	}
-	open(DIFFOUT, ">" . $DiffFile)
-		|| die "Cannot open $DiffFile for output.\n";
-}
+$Monthday = ($mday < 10 ? "0" : "") . "$mday";
+
+$SymbolicRevname = "${Release}_$Monthday-$Month-$year";
+print "Assigning symbolic revision name: $SymbolicRevname\n";
+system("rcs -n\"$SymbolicRevname\": @Files")
+    && die "Could not assign symbolic revision name. $!\n";
 
 
-foreach $File (@Files) {
-	if ($AddSymName) {
-		$Switch = ($ForceSymName ? "-N" : "-n");
-		`rcs ${Switch}${NewVersion}: $File`;
-	}
-	if ($GenerateDiff) {
-		`co -u -kv $File`;
-		open(DIFF, "rcsdiff -kv -r${OldVersion} -c2 $File | ")
-			|| die "Couldn't get an RCS diff on file: $File\n";
-
-		while ($_ = <DIFF>) {
-			print DIFFOUT $_;
-		}
-		close(DIFF);
-	}		
-}
-
-if ($GenerateDiff) {
-	close(DIFFOUT);
-}
+# Now check out all files, unlocked, with keywords removed
+system("co -u -kv @Files") && die "co -u -kv @Files failed. $!\n";
 
 
-# Now generate tar file
-if ($GenerateTar) {
-	$TarFile = "sc${MajorRev}.${MinorRev}.tar";
-	$GZTarFile = "${TarFile}.gz";
-	if (-e $TarFile) {
-		die "tar file exists: $TarFile\n" if !$OverwriteTar;
-		print "Overwriting tar file: $TarFile\n";
-		unlink $TarFile;
-	}
-	if (-e $GZTarFile) {
-		die "gzip'd tar file exists: $GZTarFile\n" if !$OverwriteTar;
-		print "Overwriting gzip'd tar file: $GZTarFile\n";
-		unlink $GZTarFile;
-	}
-	$Reporter = "reporter.el";
-	unlink $Reporter;
-	`cp ../reporter/$Reporter .`;
+# Now tar them up and compress the tar file
+print "Tar'ing and ${Compress}'ing...\n";
+@Unlinkables = split("\n", `ls ${Tarfile}* | cat`);
+#print "@Unlinkables\n";
+unlink(@Unlinkables);
 
-	$Files = join(' ', @Files, 'reporter.el');
-	`tar cvf $TarFile $Files`;
-	`gzip $TarFile`;
-}
+system("tar cvf $Tarfile @Files") && die "Couldn't tar: $!\n";
+system("$Compress $Tarfile") && die "Couldn't $Compress: $!\n";
 
 
-__END__
-### Local Variables: ;
-### mode: perl ;
-### End: ;
+print "Done.\n";
