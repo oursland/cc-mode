5c7691e19c4f7685096d36b7a0fcf3fc734bc57d
bwarsaw <devnull@localhost>
1997-03-18 16:43:13 +0000

    (c-inher-key): Require the regexp group that contains the `:'. Previously it was optional, but that broke other code.

--- cc-mode.el
+++ cc-mode.el
@@ -6,8 +6,8 @@
 ;;          1987 Dave Detlefs and Stewart Clamen
 ;;          1985 Richard M. Stallman
 ;; Created: a long, long, time ago. adapted from the original c-mode.el
-;; Version:         $Revision: 4.380 $
-;; Last Modified:   $Date: 1997-03-16 16:17:57 $
+;; Version:         $Revision: 4.381 $
+;; Last Modified:   $Date: 1997-03-18 16:43:13 $
 ;; Keywords: c languages oop
 
 ;; NOTE: Read the commentary below for the right way to submit bug reports!
@@ -1213,7 +1213,7 @@ The expansion is entirely correct because it uses the C preprocessor."
 (defconst c-inher-key
   (concat "\\(\\<static\\>\\s +\\)?"
 	  c-C++-class-key "[ \t]+" c-symbol-key
-	  "\\([ \t]*:[ \t]*\\)?\\s *[^;]")
+	  "\\([ \t]*:[ \t]*\\)\\s *[^;]")
   "Regexp describing a class inheritance declaration.")
 (defconst c-switch-label-key
   "\\(\\(case[( \t]+\\S .*\\)\\|default[ \t]*\\):"
@@ -5205,7 +5205,7 @@ command to conveniently insert and align the necessary backslashes."
 
 ;; defuns for submitting bug reports
 
-(defconst c-version "$Revision: 4.380 $"
+(defconst c-version "$Revision: 4.381 $"
   "CC Mode version number.")
 (defconst c-mode-help-address
   "bug-gnu-emacs@prep.ai.mit.edu, cc-mode-help@python.org"
