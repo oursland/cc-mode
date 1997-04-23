035cb015b01332328dc3a79d848d1119df202bf9
bwarsaw <devnull@localhost>
1997-04-23 02:56:35 +0000

    Checkpointing for removal

--- Release.py
+++ Release.py
@@ -1,8 +1,30 @@
-#! /depot/gnu/plat/bin/perl
+#! /home/bwarsaw/bin/perl
 #
 # Perl script to support public releases of cc-mode
-# $Id: Release.py,v 1.3 1994-09-10 18:38:47 warsaw Exp $
+# $Id: Release.py,v 1.4 1997-04-23 02:56:35 bwarsaw Exp $
 #
+
+# parse command line options
+$Symname = 1;
+
+while ($_ = $ARGV[0], /^-/) {
+    shift;
+    /^-nosymname$/ && ($Symname=0, next);
+    /^-(h(elp)?)|\?$/ && ($HelpP++, next);
+
+    # error
+    $ErrorP++;
+    print STDERR "Illegal switch: $_\n";
+}
+if ($HelpP || $ErrorP) {
+    print STDERR "Usage: $0",
+    "\n\t-nosymname : do not assign a symbolic name to this branch.",
+    "\n\t-help      : print this message.",
+    "\n";
+
+    exit ($ErrorP ? $ErrorP : 0);
+}
+
 
 # constants
 @Lisp = ("cc-compat", "cc-mode", "cc-mode-18");
@@ -12,6 +34,7 @@ $Main = "cc-mode.el";
 $Tarfile = "cc-mode-XXX.tar";
 $Release = "X--------------------->Full";
 $Compress = "gzip";
+$Tmpdir = "./tmp$$";
 
 
 # Calculate the new version number
@@ -30,22 +53,35 @@ die "The following distribution files are still locked: @Locks\n"
     if @Locks;
 
 
-# set the symbol name for the branch heads of all the files.
-($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+if ($Symname) {
+    # set the symbol name for the branch heads of all the files.
+    ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
 
-$Month = ("Jan", "Feb", "Mar", "Apr", "May", "Jun",
-	  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")[$mon];
+    $Month = ("Jan", "Feb", "Mar", "Apr", "May", "Jun",
+	      "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")[$mon];
 
-$Monthday = ($mday < 10 ? "0" : "") . "$mday";
+    $Monthday = ($mday < 10 ? "0" : "") . "$mday";
 
-$SymbolicRevname = "${Release}_$Monthday-$Month-$year";
-print "Assigning symbolic revision name: $SymbolicRevname\n";
-system("rcs -n\"$SymbolicRevname\": @Files")
-    && die "Could not assign symbolic revision name. $!\n";
+    $SymbolicRevname = "${Release}_$Monthday-$Month-$year";
+    print "Assigning symbolic revision name: $SymbolicRevname\n";
+    system("rcs -n\"$SymbolicRevname\": @Files")
+	&& die "Could not assign symbolic revision name. $!\n";
+}
+else {
+    print "Skipping assignment of symbolic revision name.\n";
+}
 
 
-# Now check out all files, unlocked, with keywords removed
+# Now check out all files, unlocked, with keywords removed.  Create a
+# temporary directory and move the files to that directory.  We do
+# this because we're going to hack the permissions before they go into
+# the tar file.
 system("co -u -kv @Files") && die "co -u -kv @Files failed. $!\n";
+
+mkdir($Tmpdir, 750) || die "Could not make temporary directory: $Tmpdir\n";
+system("cp @Files $Tmpdir") && die "Could not copy files to: $Tmpdir\n";
+chdir($Tmpdir) || die "Could not change to directory: $Tmpdir\n";
+chmod(644, @Files) || die "Could not change modes\n";
 
 
 # Now tar them up and compress the tar file
@@ -56,6 +92,11 @@ unlink(@Unlinkables);
 
 system("tar cvf $Tarfile @Files") && die "Couldn't tar: $!\n";
 system("$Compress $Tarfile") && die "Couldn't $Compress: $!\n";
+system("mv ${Tarfile}* ..");
 
+# clean up temp files
+unlink(@Files);
+chdir("..") || die "Could not chdir to parent (..)\n";
+rmdir($Tmpdir) || die "Could not rmdir: $Tmpdir\n";
 
 print "Done.\n";
