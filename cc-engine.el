ef7398b0889f339f3c79769c9545c1883e31ddfd
bwarsaw <devnull@localhost>
1997-04-25 21:58:30 +0000

    Initial revision

---
+++ cc-engine.el
@@ -0,0 +1,31 @@
+;;; cc-engine.el --- core syntax guessing engine for CC mode
+
+;; Copyright (C) 1997 Free Software Foundation, Inc.
+
+;; Authors:  1992-1997 Barry A. Warsaw
+;;           1987 Dave Detlefs and Stewart Clamen
+;;           1985 Richard M. Stallman
+;; Created:  22-Apr-1997 (split from cc-mode.el)
+;; Version:  5.00
+;; Keywords: c languages oop
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+
+(provide 'cc-engine)
+;;; cc-engine.el ends here
