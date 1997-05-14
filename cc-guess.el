867f814efe5c4f7b9a7c95c0e6c830bdb6523167
bwarsaw <devnull@localhost>
1997-05-14 22:19:32 +0000

    #Bumping to release revision 5.06

--- cc-guess.el
+++ cc-guess.el
@@ -5,7 +5,7 @@
 ;; Author:     1994-1995 Barry A. Warsaw
 ;; Maintainer: Unmaintained
 ;; Created:    August 1994, split from cc-mode.el
-;; Version:    5.05
+;; Version:    5.06
 ;; Keywords:   c languages oop
 
 ;; This file is not part of GNU Emacs.
