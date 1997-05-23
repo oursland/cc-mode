6033cb2fad3ae6b8b3e6e0c25a74d34907d5598c
bwarsaw <devnull@localhost>
1997-05-23 14:58:30 +0000

    Added a require of the custom library

--- cc-vars.el
+++ cc-vars.el
@@ -27,6 +27,8 @@
 ;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 ;; Boston, MA 02111-1307, USA.
 
+(require 'custom)
+
 
 (defcustom c-strict-syntax-p nil
   "*If non-nil, all syntactic symbols must be found in `c-offsets-alist'.
