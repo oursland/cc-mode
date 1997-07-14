f080e719ac511dca061fe6fc718f384b93885dc8
bwarsaw <devnull@localhost>
1997-07-14 22:54:25 +0000

    Added (require 'cc-defs) to pick up the definition of c-emacs-features.

--- cc-langs.el
+++ cc-langs.el
@@ -27,6 +27,8 @@
 ;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 ;; Boston, MA 02111-1307, USA.
 
+(require 'cc-defs)
+
 
 ;; Regular expressions and other values which must be parameterized on
 ;; a per-language basis.
