23db6af05d7a8c456a0598ce96f1f0a4d8007d00
bwarsaw <devnull@localhost>
1997-07-17 22:26:09 +0000

    #added path-to-the-custom-library

--- cc-make.el
+++ cc-make.el
@@ -1,2 +1,11 @@
+(setq path-to-the-custom-library
+      ;; Change the following line to the directory that contains
+      ;; Per's new custom library.  Leave this as nil if you are using
+      ;; XEmacs 20.3, Emacs 20.0 or beyond.
+      nil
+      )
+
 (setq load-path (cons "./" load-path))
+(if path-to-the-custom-library
+    (setq load-path (cons path-to-the-custom-library load-path)))
 (batch-byte-compile)
