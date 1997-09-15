eca2911f89580731528187d7007be95352df99e7
bwarsaw <devnull@localhost>
1997-09-15 23:31:17 +0000

    #Better check for defcustom, and more obnoxious warning message.

--- cc-make.el
+++ cc-make.el
@@ -15,6 +15,14 @@
 (setq load-path (cons "./" load-path))
 (if path-to-the-custom-library
     (setq load-path (cons path-to-the-custom-library load-path)))
-(or (require 'custom)
-    (error "STOP!  Custom library not found.  You MUST fix cc-make.el"))
-(batch-byte-compile)
+(if (and (condition-case nil
+	     (require 'custom)
+	   (error nil))
+	 ;; Stock Emacs 19.34 doesn't have this
+	 (fboundp 'defcustom))
+    (batch-byte-compile)
+  (error "STOP! STOP! STOP! STOP!
+
+The Custom library was not found or is out of date.  A more current
+version is required to use CC Mode 5.  You MUST fix cc-make.el.  See
+that file or the CC Mode README for details."))
