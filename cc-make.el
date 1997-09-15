aecf28005e82247a4044a9ed84a4cbb3287e112a
bwarsaw <devnull@localhost>
1997-09-15 13:43:33 +0000

    Signal an error if the Custom library could not be found

--- cc-make.el
+++ cc-make.el
@@ -2,12 +2,19 @@
 
 (setq path-to-the-custom-library
       ;; In Emacs 19.34, change the following line to the directory
-      ;; that contains Per's new custom library, which you should have
-      ;; already downloaded.
+      ;; that contains Per Abrahamsen's new Custom library, which you
+      ;; must download first.  You can get Custom from:
+      ;;
+      ;; <http://www.dina.kvl.dk/~abraham/custom/>
+      ;;
+      ;; See CC Mode's README file for details (also available at
+      ;; <ftp://ftp.python.org/pub/emacs/cc-mode/README>)
       nil
       )
 
 (setq load-path (cons "./" load-path))
 (if path-to-the-custom-library
     (setq load-path (cons path-to-the-custom-library load-path)))
+(or (require 'custom)
+    (error "STOP!  Custom library not found.  You MUST fix cc-make.el"))
 (batch-byte-compile)
