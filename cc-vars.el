5763749f4cb82ce2dd1d1a21d2814aba90ac07a0
bwarsaw <devnull@localhost>
1997-10-06 19:03:53 +0000

    (c-progress-interval): Document new semantics

--- cc-vars.el
+++ cc-vars.el
@@ -304,8 +304,8 @@ enforces this."
 (defcustom c-progress-interval 5
   "*Interval used to update progress status during long re-indentation.
 If a number, percentage complete gets updated after each interval of
-that many seconds.  Set to nil to inhibit updating.  This is only
-useful for Emacs 19."
+that many seconds.  To inhibit all messages during indentation, set
+this variable to nil."
   :type 'integer
   :group 'c)
 
