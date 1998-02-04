15b23be773b8428ae1193ac5df1292f733ce5f17
bwarsaw <devnull@localhost>
1998-02-04 22:22:26 +0000

    #Python program which gets about 80% of the way to generating `proper' #ChangeLog entries from the CVS logs.  Still could use work.  80/20 #rule applies!

---
+++ Update.py
@@ -0,0 +1,159 @@
+"""ChangeLog updater.  X/Emacs's rcs2log is just f*cking broken.
+
+Rather than try to fix the brokenness in rcs2log and vc-update-change-log,
+this Python script will get you 90% of the way toward converting the CVS logs
+to ChangeLog format.  I took a few liberties with the ChangeLog format, namely
+eliminating the time field, but I still like my format better than the ISO8106
+format that VC uses by default.
+
+The output of this script will still have to be munged somewhat by hand.
+Paragraphs will have to be wrapped manually, and similar file changes will
+have to be collapsed.  That's not a huge penalty because the CVS logs always
+have to be, er, sanitized (proofread) before sending to RMS.
+
+This script does properly ignore `#' leading log messages.
+"""
+
+import os
+import string
+import re
+import time
+import pwd
+
+
+date_re = re.compile(
+    '^'
+    '(?P<dow>Mon|Tue|Wed|Thu|Fri|Sat|Sun)\s+'
+    '(?P<mon>Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+'
+    '(?P<day>\d{1,2})\s+'
+    '(?P<time>\d{2}:\d{2}:\d{2})\s+'
+    '(?P<year>\d{4})\s+'
+    )
+
+
+def get_last_date(filename):
+    fp = open(filename)
+    while 1:
+	line = fp.readline()
+	if not line:
+	    break
+	mo = date_re.match(line)
+	if mo:
+	    fp.close()
+	    return mo.group('dow', 'mon', 'day', 'time', 'year')
+    return None
+
+
+
+filere = re.compile('^Working file: (?P<file>.*)')
+revre = re.compile('^revision \d.\d')
+logdate_re = re.compile(
+    '^date: (?P<year>\d{4})/(?P<mon>\d{2})/(?P<day>\d{2})\s+'
+    '(?P<time>\d{2}:\d{2}:\d{2});\s+'
+    'author: (?P<name>[^;]+);\s+'
+    #state: Exp;
+    )
+
+
+def read_revision(fp):
+    revision = fp.readline()
+    dateline = fp.readline()
+    mo = logdate_re.match(dateline)
+    date = tuple(map(int, mo.group('year', 'mon', 'day')))
+    # now collect change log entry
+    logmsg = ''
+    endof_file = 0
+    while 1:
+	line = fp.readline()
+	if not line or \
+	   line[:28] == '----------------------------' or \
+	   line[:28] == '============================':
+	    break
+	# look for `comment'
+	if line[0] == '#':
+	    continue
+	logmsg = logmsg + line[:-1] + ' '
+    if line[:28] == '============================':
+	endof_file = 1
+    return (endof_file, logmsg, date, mo.group('name'))
+
+
+def get_log_entries(dow, mon, day, time, year):
+    cvscmd = 'cvs log -d">%s-%s-%s %s"' % (day, mon, year, time)
+    fp = os.popen(cvscmd, 'r')
+
+    changes = {}
+    while 1:
+	line = fp.readline()
+	if not line:
+	    break
+	# the name of the file??
+	mo = filere.match(line)
+	if mo:
+	    filename = mo.group('file')
+	    continue
+	if line[:28] == '----------------------------':
+	    while 1:
+		endof_file, logmsg, date, name = read_revision(fp)
+		if logmsg:
+		    todays_changes = changes.get(date, [])
+		    todays_changes.append((logmsg, name, filename))
+		    changes[date] = todays_changes
+		if endof_file:
+		    break
+    # all done now
+    fp.close()
+    return changes
+
+
+
+def group_by_user(changes):
+    changes_bydate = {}
+    for date in changes.keys():
+	changes_byuser = {}
+	for logmsg, name, filename in changes[date]:
+	    byuser = changes_byuser.get(name, [])
+	    byuser.append((filename, logmsg))
+	    changes_byuser[name] = byuser
+	changes_bydate[date] = changes_byuser
+    return changes_bydate
+
+
+
+# older dates come last
+def datecmp(date1, date2):
+    # compare years
+    if date1[0] == date2[0]:
+	# compare months
+	if date1[1] == date2[1]:
+	    # compare days
+	    return cmp(date2[2], date1[2])
+	else:
+	    return cmp(date2[1], date1[1])
+    else:
+	return cmp(date2[0], date1[0])
+    
+
+
+if __name__ == '__main__':
+    usernames = {}
+
+    lastchange = get_last_date('ChangeLog')
+    changes = apply(get_log_entries, lastchange)
+    changes = group_by_user(changes)
+
+    dates = changes.keys()
+    dates.sort(datecmp)
+    for date in dates:
+	timetuple = tuple(date) + (0, 0, 0, 0, 0, 0)
+	timestr = time.strftime('%a %b %d %Y', timetuple)
+
+	changes_byuser = changes[date]
+	for name in changes_byuser.keys():
+	    longname = usernames.get(name, None)
+	    if not longname:
+		longname = pwd.getpwnam(name)[4]
+		usernames[name] = longname
+	    print '\n%s  %s  <cc-mode-help@python.org>' % (timestr, longname)
+	    for filename, logmsg in changes_byuser[name]:
+		print '\n\t*', filename, logmsg
