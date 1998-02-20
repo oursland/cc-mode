ee84247710877bbcfb5ce9954953104df802acd8
bwarsaw <devnull@localhost>
1998-02-20 00:51:29 +0000

    #Removed the require of cc-defs

--- cc-langs.el
+++ cc-langs.el
@@ -27,7 +27,6 @@
 ;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 ;; Boston, MA 02111-1307, USA.
 
-(require 'cc-defs)
 
 
 ;; Regular expressions and other values which must be parameterized on
