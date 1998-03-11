81557b5e340f5eabd720b41c36449af612e139dc
bwarsaw <devnull@localhost>
1998-03-11 23:22:13 +0000

    (c-forward-token-1, c-backward-token-1): Let-bind signal-error-on-buffer-boundary to t so that backward-char works like it's always worked.  This brain-damaged variable only exists in XEmacs.

--- cc-engine.el
+++ cc-engine.el
@@ -287,7 +287,9 @@
 (defun c-forward-token-1 (&optional count balanced lim)
   (let* ((jump-syntax (if balanced
 			  '(?w ?_ ?\" ?\\ ?/ ?$ ?' ?\( ?\))
-			'(?w ?_ ?\" ?\\ ?/ ?$ ?'))))
+			'(?w ?_ ?\" ?\\ ?/ ?$ ?')))
+	 ;; this is a XEmacs built-in that wreaks havoc
+	 (signal-error-on-buffer-boundary t))
     (or count (setq count 1))
     (condition-case nil
 	(while (progn
@@ -305,6 +307,8 @@
   (let* ((jump-syntax (if balanced
 			  '(?w ?_ ?\" ?\\ ?/ ?$ ?' ?\( ?\))
 			'(?w ?_ ?\" ?\\ ?/ ?$ ?')))
+	 ;; this is a XEmacs built-in that wreaks havoc
+	 (signal-error-on-buffer-boundary t)
 	 last)
     (or count (setq count 1))
     (condition-case nil
