5b78d5b0df15bebd4e2b731d04e6933fcc7af6cb
bwarsaw <devnull@localhost>
1998-03-29 16:13:46 +0000

    *** empty log message ***

---
+++ admin/links.h
@@ -0,0 +1,4 @@
+<li><a href="release.html">Current Release</a>
+<li><a href="changes.html">Recent Changes</a>
+<li><a href="installation.html">Installation Notes</a>
+<li><a href="compat.html">Compatibility Notes</a>
