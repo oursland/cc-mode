15d43309582e9e54294dc834d86f68d7579d6273
bwarsaw <devnull@localhost>
1998-03-30 17:39:52 +0000

    Initial revision

---
+++ admin/changelinks.h
@@ -0,0 +1,11 @@
+<!-- -*- html -*- -->
+<h3>Versions</h3>
+<li><a href="changes-521.html">Changes for 5.21</a>
+<li><a href="changes-520.html">Changes for 5.20</a>
+<li><a href="changes-519.html">Changes for 5.19</a>
+<li><a href="changes-518.html">Changes for 5.18</a>
+<li><a href="changes-517.html">Changes for 5.17</a>
+<li><a href="changes-516.html">Changes for 5.16</a>
+<li><a href="changes-515.html">Changes for 5.15</a>
+<li><a href="changes-514.html">Changes for 5.14</a>
+<li><a href="changes-v4.html">Changes b/w v4 and v5</a>
