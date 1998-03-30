fb79b4c0281e433cfa67226f5bb45ab70559cf67
bwarsaw <devnull@localhost>
1998-03-30 17:46:09 +0000

    Added liks

--- admin/compatlinks.h
+++ admin/compatlinks.h
@@ -1,1 +1,6 @@
 <!-- -*- html -*- -->
+<h3>Emacsen</h3>
+<li><a href="compat_xemacs.html">XEmacs, Emacs 20</a>
+<li><a href="compat_emacs19.html">Emacs 19</a>
+<li><a href="compat_emacs18.html">Emacs 18</a>
+<li><a href="compat_delbs_awk.html">Other Elisp</a>
