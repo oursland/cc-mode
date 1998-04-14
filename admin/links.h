e1776b53fe22c555efc54a3ae0f7e03762f8a1dd
bwarsaw <devnull@localhost>
1998-04-14 03:53:27 +0000

    A couple of mds

--- admin/links.h
+++ admin/links.h
@@ -1,9 +1,10 @@
 <!-- -*- html -*- -->
 <h3>CC Mode</h3>
 <li><a href="index.html">Index</a>
-<li><a href="release.html">Current Release</a>
-<li><a href="changes-521.html">Recent Changes</a>
-<li><a href="installation.html">Installation Notes</a>
-<li><a href="compat.html">Compatibility Notes</a>
-<li><a href="contrib.html">Contributed Stuff</a>
+<li><a href="release.html">Current source release</a>
+<li><a href="cc-mode.html/Top.html">Browse the online docs</a>
+<li><a href="changes-521.html">Recent changes</a>
+<li><a href="installation.html">Installation notes</a>
+<li><a href="compat.html">Compatibility notes</a>
+<li><a href="contrib.html">Contributed stuff</a>
 <li><a href="../">Back to Emacs Goodies</a>
