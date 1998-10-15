bf5c98b5c8a327c5e54422aa9ed531cdeb7f6d4b
bwarsaw <devnull@localhost>
1998-10-15 21:00:29 +0000

    *** empty log message ***

--- admin/links.h
+++ admin/links.h
@@ -2,8 +2,8 @@
 <h3>CC Mode</h3>
 <li><a href="index.html">Index</a>
 <li><a href="release.html">Current source release</a>
-<li><a href="cc-mode.html/Top.html">Browse the online docs</a>
-<li><a href="changes-521.html">Recent changes</a>
+<li><a href="cc-mode-html/Top.html">Browse the online docs</a>
+<li><a href="changes-524.html">Recent changes</a>
 <li><a href="installation.html">Installation notes</a>
 <li><a href="compat.html">Compatibility notes</a>
 <li><a href="contrib.html">Contributed stuff</a>
