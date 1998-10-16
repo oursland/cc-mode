d1d59b56ce7599568eb106ad79921f5d4d2ce970
bwarsaw <devnull@localhost>
1998-10-16 02:03:43 +0000

    #Bumping to CC Mode 5.25

--- cc-mode.el
+++ cc-mode.el
@@ -10,7 +10,7 @@
 ;; Created:    a long, long, time ago. adapted from the original c-mode.el
 ;; Keywords:   c languages oop
 
-(defconst c-version "5.24"
+(defconst c-version "5.25"
   "CC Mode version number.")
 
 ;; NOTE: Read the commentary below for the right way to submit bug reports!
