092ce097ec454c48589d56ff1ac67670f290a12c
mast <devnull@localhost>
1998-12-07 05:15:08 +0000

    #Test more tricky character cases.

--- tests/bracelist-11.cc
+++ tests/bracelist-11.cc
@@ -1,5 +1,9 @@
+#define tricky <>:;""''#
+#define tricky2 \
+  foo \
+  <>:;""''#
 #include <stdio.h>
-
+/* <>:;"'# */// <>:;"'# comments with tricky characters
 const char *words[] = {
     "hello",
     "there",
