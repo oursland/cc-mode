4c9a7269611734dc56aa4bf731554b1c55a7ec88
bwarsaw <devnull@localhost>
1998-12-07 18:25:28 +0000

    *** empty log message ***

--- admin/changelinks.h
+++ admin/changelinks.h
@@ -1,5 +1,6 @@
 <!-- -*- html -*- -->
 <h3>Versions</h3>
+<li><a href="changes-524.html">Changes for 5.24</a>
 <li><a href="changes-521.html">Changes for 5.21</a>
 <li><a href="changes-520.html">Changes for 5.20</a>
 <li><a href="changes-519.html">Changes for 5.19</a>
