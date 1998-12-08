672f60586232a26340d68e77963331116a9d4366
bwarsaw <devnull@localhost>
1998-12-08 01:26:51 +0000

    No changelog

--- admin/links.h
+++ admin/links.h
@@ -4,7 +4,6 @@
 <li><a href="release.html">Current source release</a>
 <li><a href="cc-mode-html/Top.html">Browse the online docs</a>
 <li><a href="changes-525.html">Recent changes</a>
-<li><a href="ChangeLog.gz">ChangeLog file</a>
 <li><a href="installation.html">Installation notes</a>
 <li><a href="compat.html">Compatibility notes</a>
 <li><a href="contrib.html">Contributed stuff</a>
