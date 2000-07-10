8164589d794dbb9a533e580d9074e128560d8cf6
mast <devnull@localhost>
2000-07-10 02:14:51 +0000

    #Linguistic fixes.

--- admin/anoncvs.php
+++ admin/anoncvs.php
@@ -5,14 +5,14 @@
 ?>
 
 <p>The current CC Mode development version is always available through
-anonymous CVS.  Being under development, there's of course not any
+anonymous CVS.  Being under development, there's of course no
 guarantees that it will work well, or at all.  That said, it usually
 works well, maybe even better than the last release since bug fixes
-get into it first.
+get there first.
 
 <p>You're welcome to report bugs, opinions and patches regarding the
-development version.  It's very likely that pilot error questions go
-by unanswered, however.
+development version.  However, it's very likely that pilot error
+questions will go by unanswered.
 
 <h3>Checking out the CVS tree</h3>
 
@@ -30,6 +30,6 @@ by unanswered, however.
 You'll find the regression test suite in the <code>tests</code>
 directory; take a look in <code>000tests.el</code> to figure out how
 to use it.  The <code>admin</code> directory probably isn't very
-interesting; it contains the source for this web site.
+interesting; it just contains the source for this web site.
 
 <?php include ("footer.h"); ?>
