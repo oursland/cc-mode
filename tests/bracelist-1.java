c2e3d683855cad1152292af4e914f73fa1cf30d1
mast <devnull@localhost>
2001-02-05 01:51:02 +0000

    #Modification since "new Foo[]" initializer brace lists now are treated as #expressions and therefore gets indented one step more.

--- tests/bracelist-1.java
+++ tests/bracelist-1.java
@@ -12,12 +12,12 @@ public class Foo {
 	},
     };
     public String arr1[][] = new String[][]
-    {
-	{"d1", "d2",},
-	{"d1",
-	 "d2"
-	}
-    };
+	{
+	    {"d1", "d2",},
+	    {"d1",
+	     "d2"
+	    }
+	};
     public String fun()
     {
 	a = new b[2];
