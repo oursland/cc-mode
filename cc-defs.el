8e715f171a7fcf66014ddf738e377710e0d023ad
mast <devnull@localhost>
2001-07-24 17:57:14 +0000

    #Moved it to the macro section.

--- cc-defs.el
+++ cc-defs.el
@@ -193,6 +193,19 @@
        (set-syntax-table c-with-syntax-table-orig-table))))
 (put 'c-with-syntax-table 'lisp-indent-function 1)
 
+;; Make edebug understand the macros.
+(eval-after-load "edebug"
+  (progn
+    (def-edebug-spec c-paren-re t)
+    (def-edebug-spec c-identifier-re t)
+    (def-edebug-spec c-point ([&or symbolp form] &optional form))
+    (def-edebug-spec c-safe t)
+    (def-edebug-spec c-forward-sexp (&optional [&or numberp form]))
+    (def-edebug-spec c-backward-sexp (&optional [&or numberp form]))
+    (def-edebug-spec c-add-syntax t)
+    (def-edebug-spec c-add-class-syntax t)
+    (def-edebug-spec c-with-syntax-table t)))
+
 ;;; Inline functions.
 
 ;; Note: All these after the macros, to be on safe side in avoiding
@@ -343,19 +356,6 @@
 
 (defsubst c-major-mode-is (mode)
   (eq (derived-mode-class major-mode) mode))
-
-;; Make edebug understand the macros.
-(eval-after-load "edebug"
-  (progn
-    (def-edebug-spec c-paren-re t)
-    (def-edebug-spec c-identifier-re t)
-    (def-edebug-spec c-point ([&or symbolp form] &optional form))
-    (def-edebug-spec c-safe t)
-    (def-edebug-spec c-forward-sexp (&optional [&or numberp form]))
-    (def-edebug-spec c-backward-sexp (&optional [&or numberp form]))
-    (def-edebug-spec c-add-syntax t)
-    (def-edebug-spec c-add-class-syntax t)
-    (def-edebug-spec c-with-syntax-table t)))
 
 
 (cc-provide 'cc-defs)
