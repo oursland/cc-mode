e49e681fd865fd71319102c5a9f158cdfb3812fc
mast <devnull@localhost>
2001-10-06 02:06:17 +0000

    #Added some more tests of blocks in macro arguments.

--- tests/inexprstat-4.c
+++ tests/inexprstat-4.c
@@ -16,4 +16,19 @@ int foo() {
 	 if (a)
 	     x;
 	);
+    NIFTY_LITTLE_KANGAS ({
+			     x;
+			     y;
+			 },
+			 b);
+    NIFTY_LITTLE_KANGAS (a, {
+			     x;
+			     y;
+			 },
+			 b);
+    NIFTY_LITTLE_KANGAS (a, { x;
+			     y;
+			     z;
+			 },
+			 b);
 }
