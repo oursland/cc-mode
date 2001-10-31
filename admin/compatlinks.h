3079346f9c0c1016d8eee760f7f8c1e5d84f0153
mast <devnull@localhost>
2001-10-31 22:44:28 +0000

    #Update since there's now an Emacs 21.

--- admin/compatlinks.h
+++ admin/compatlinks.h
@@ -1,6 +1,6 @@
 <!-- -*- html -*- -->
 <h4>Compatibility</h4>
-<a href="compat_xemacs.php">XEmacs, Emacs 20</a><br />
+<a href="compat_xemacs.php">XEmacs, Emacs &gt;= 20</a><br />
 <a href="compat_emacs19.php">Emacs 19</a><br />
 <a href="compat_emacs18.php">Emacs 18</a><br />
 <a href="compat_delbs_awk.php">Other Elisp</a><br />
