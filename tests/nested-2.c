82608ec66ccac54f1421e814f8e1c5813ec47a49
mast <devnull@localhost>
2002-01-20 21:40:54 +0000

    #Change since the gcc extension of nested functions is handled better.

--- tests/nested-2.c
+++ tests/nested-2.c
@@ -2,9 +2,9 @@ double
 hypotenuse (double a, double b)
 {
     double square (double x)
-        {
-            return x * x;
-        }
+    {
+	return x * x;
+    }
 
     return sqrt (square (a) + square (b));
 }
