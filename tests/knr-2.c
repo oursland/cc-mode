a12aeea726ac34f0ce759806e19fdbee57d77048
mast <devnull@localhost>
2002-03-30 13:37:57 +0000

    #Added some K&R argdecl tests.

---
+++ tests/knr-2.c
@@ -0,0 +1,10 @@
+
+DEFUN ("lax-plist-put", Flax_plist_put, Slax_plist_put, 3, 3, 0,
+       doc: /* ...
+	     ... */)
+    (plist, prop, val)
+    Lisp_Object plist;
+    register Lisp_Object prop;
+    Lisp_Object val;
+{
+}
