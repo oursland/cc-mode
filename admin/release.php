04bd714a23cbc3e08fea3fd275a9d9cff8a0dc9e
mast <devnull@localhost>
2002-04-01 02:20:43 +0000

    #Some markup fixes. (Really just another test of the cvs mailer.)

--- admin/release.php
+++ admin/release.php
@@ -10,17 +10,18 @@ texinfo format as an approximately <a
 href="http://download.sourceforge.net/cc-mode/cc-mode-5.28.tar.gz">160KB
 gzip'd tarball</a> (also available <a
 href="ftp://ftp.sourceforge.net/pub/sourceforge/cc-mode/cc-mode-5.28.tar.gz">via
-FTP</a>).  You can browse the <a href="src/">individual files</a> too.
+FTP</a>).  You can browse the <a href="src/">individual files</a>
+too.</p>
 
 <p>The documentation is also available in several forms.  You can
 either <a href="cc-mode.html#Top">browse the on-line documentation</a>
 or grab one of the several <a href="manual/">pre-formatted</a>
 documents generated from the texinfo source, including PostScript,
-Emacs Info, and HTML versions</a>.
+Emacs Info, and HTML versions</a>.</p>
 
 <p>Since version 5.26, CC Mode supports using Filladapt mode to fill
 text in comments and string literals.  There are however still some <a
 href="filladapt.php">issues</a> with this that you should be aware of
-if you want to use Filladapt in CC Mode.
+if you want to use Filladapt in CC Mode.</p>
 
 <?php include ("footer.h"); ?>
