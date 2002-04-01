c484d0a467a66e34cd29619df8bd0ac36dd09195
mast <devnull@localhost>
2002-04-01 02:02:43 +0000

    #Fixed some html markup, mostly to see if the cc-mode-checkins list #works.

--- admin/index.php
+++ admin/index.php
@@ -6,24 +6,24 @@
 <p><strong>CC Mode is an Emacs and XEmacs mode for editing C and other
 languages with similar syntax; currently C++, Objective-C, Java,
 CORBA's IDL, and Pike. It's a standard package in both Emacs and
-XEmacs.</strong>
+XEmacs.</strong></p>
 
 <p>CC Mode is brought to you by Martin Stjernholm, the current CC Mode
 maintainer.  Several people before him have helped make CC Mode what
 it is today, notably Barry Warsaw who took over of BOCM (Boring Old C
-Mode) back in 1992 and revamped it to CC Mode.
+Mode) back in 1992 and revamped it to CC Mode.</p>
 
 <p>The <a href="release.php">current release</a> is version
 5.28.  A list of <a href="changes-latest.php">recent changes</a> are
 given in detail, as well as some <a
 href="installation.php">installation notes</a> and Emacs/XEmacs <a
-href="compat.php">compatibility details</a>.
+href="compat.php">compatibility details</a>.</p>
 
 <p>You can <a href="cc-mode.html#Top">browse the documentation
 on-line</a>.  There are also some (recently) archived <a
-href="lists.php">mailing lists</a> which you can browse and join.
+href="lists.php">mailing lists</a> which you can browse and join.</p>
 
 <p>See also <a href="contrib.php">some related Emacs packages</a> that
-have been contributed by CC Mode users.
+have been contributed by CC Mode users.</p>
 
 <?php include ("footer.h"); ?>
