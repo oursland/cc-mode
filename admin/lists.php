d6c2fbb6286bf74d6816cec71b6704ec310248a8
mast <devnull@localhost>
2002-04-20 15:47:00 +0000

    #Updated the link to the cc-mode-help archive.

--- admin/lists.php
+++ admin/lists.php
@@ -9,7 +9,7 @@ href="http://lists.sourceforge.net/mailman/listinfo/cc-mode-announce">join</a>
 the announcement-only mailing list.
 
 <p>You can also <a
-href="http://www.geocrawler.com/redir-sf.php3?list=cc-mode-help">browse</a>
+href="http://sourceforge.net/mailarchive/forum.php?forum_id=6827">browse</a>
 or <a
 href="http://lists.sourceforge.net/mailman/listinfo/cc-mode-help">join</a>
 the general mailing list for CC Mode, which gets all the bug reports,
