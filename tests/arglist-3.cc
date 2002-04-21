b52696f34df3a3a9fa80cd8633eb6850cdc7884b
mast <devnull@localhost>
2002-04-21 00:11:51 +0000

    #No longer indents according to the comment after the topmost-intro-cont #change.  I ignore that since the comment doesn't motivates why it should #indent like it did before.

--- tests/arglist-3.cc
+++ tests/arglist-3.cc
@@ -3,10 +3,10 @@ class Frog
 private:
 
     int a 
-    (                   // this parend should line up with 'int a'
-        int arg1,
-        int arg2
-        );
+	(			// this parend should line up with 'int a'
+	    int arg1,		// Can't see any reason for the comment
+	    int arg2		// above, so I ignore it. :\  /mast
+	    );
 
     int b (
         int arg1,
