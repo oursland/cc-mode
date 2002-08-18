89b1380223583f2e82126496c439c592f04ee984
mast <devnull@localhost>
2002-08-18 14:40:24 +0000

    #The preference in one case was tuned more towards a function call than a #declaration.

--- tests/decls-6.cc
+++ tests/decls-6.cc
@@ -2,13 +2,13 @@ Type var = init, x = Type();
 Type (*var) = init;
 Type var[3 * peq] = init;
 Type[xyz] var = init;		// FIXME: Java only?
-Type (var) = init;		// Currently interpreted as a function call.
+Type (var) = init;		// Currently recognized as a function call.
 Type int = "int";		// int
 
 const Type var;
 const Type (*var);
 const Type var[3 * peq];
-const Type (var);		// Recognized as decl due to the const keyword.
+const Type (var);		// Currently recognized as a function call.
 
 Type (*foo) (Type *,
 	     Type (*)[x],
