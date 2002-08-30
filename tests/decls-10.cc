52e3102a30b790bf44b52e5d7185fb9d66220818
mast <devnull@localhost>
2002-08-30 14:13:47 +0000

    #Test fontification of tricky expression/declaration cases.

---
+++ tests/decls-10.cc
@@ -0,0 +1,24 @@
+int main() {
+    t1 (id) = 1;
+    t2 (*id) = NULL;
+    t3 (id)[n] = {1};
+    t4 (*id)[n] = NULL;
+
+    t1 (id) (1);
+    v1 (id) (1);
+
+    t2 (*id) (NULL);
+    v2 (*id) (NULL);
+    t5  *id  (NULL);
+
+    t3 (*id) (NULL) + 1;
+    v3 (*id) (NULL) + 1;
+    t6  *id  (NULL) + 1;	// Invalid syntax.
+
+    v4 ();
+    t5 (*);			// Not valid in a block.
+    t6 ()[n];			// Not valid in a block.
+    v5 (id);
+    v6 (id)[n];
+    v7 [n];
+}
