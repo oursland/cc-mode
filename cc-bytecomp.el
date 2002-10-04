f7d5857e8a21e58cdb568b2f81d7886d8ee27935
mast <devnull@localhost>
2002-10-04 23:30:42 +0000

    (cc-eval-when-compile): Workaround for a bug with nested `eval-when-compile' in XEmacs 21.

--- cc-bytecomp.el
+++ cc-bytecomp.el
@@ -45,45 +45,60 @@
 (defvar cc-bytecomp-load-depth 0)
 (defvar cc-bytecomp-loaded-files nil)
 (defvar cc-bytecomp-environment-set nil)
+(defvar cc-bytecomp-inside-eval-when-compile nil)
 
 (put 'cc-eval-when-compile 'lisp-indent-hook 0)
 (defmacro cc-eval-when-compile (&rest body)
   "Like `progn', but evaluates the body at compile time.
 The result of the body appears to the compiler as a quoted constant.
 
-This variant works around what looks like a bug in
-`eval-when-compile': During byte compilation it byte compiles its
-contents before evaluating it.  That can cause forms to be compiled in
-situations they aren't intended to be compiled.  See cc-bytecomp.el
-for further discussion."
-  ;;
-  ;; Example: It's not possible to defsubst a primitive, e.g. the
-  ;; following will produce an error (in any emacs flavor), since
-  ;; `nthcdr' is a primitive function that's handled specially by the
-  ;; byte compiler and thus can't be redefined:
-  ;;
-  ;;     (defsubst nthcdr (val) val)
-  ;;
-  ;; `defsubst', like `defmacro', needs to be evaluated at compile
-  ;; time, so this will produce an error during byte compilation.
-  ;;
-  ;; CC Mode occasionally needs to do things like this for cross-emacs
-  ;; compatibility (although we try to avoid it since it results in
-  ;; byte code that isn't compatible between emacsen).  It therefore
-  ;; uses the following to conditionally do a `defsubst':
-  ;;
-  ;;     (eval-when-compile
-  ;;       (if (not (fboundp 'foo))
-  ;;           (defsubst foo ...)))
-  ;;
-  ;; But `eval-when-compile' byte compiles its contents and _then_
-  ;; evaluates it (in all current emacs versions, up to and including
-  ;; Emacs 20.6 and XEmacs 21.1 as of this writing).  So this will
-  ;; still produce an error, since the byte compiler will get to the
-  ;; defsubst anyway.  That's arguably a bug because the point with
-  ;; `eval-when-compile' is that it should evaluate rather than
-  ;; compile its contents.
-  `(eval-when-compile (eval '(progn ,@body))))
+This variant works around bugs in `eval-when-compile' in various
+\(X)Emacs versions.  See cc-bytecomp.el for details."
+
+  (if cc-bytecomp-inside-eval-when-compile
+      ;; XEmacs 21.4.6 has a bug in `eval-when-compile' in that it
+      ;; evaluates its body at macro expansion time if it's nested
+      ;; inside another `eval-when-compile'.  So we use a dynamically
+      ;; bound variable to avoid nesting them.
+      `(progn ,@body)
+
+    `(eval-when-compile
+       ;; In all (X)Emacsen so far, `eval-when-compile' byte compiles
+       ;; its contents before evaluating it.  That can cause forms to
+       ;; be compiled in situations they aren't intended to be
+       ;; compiled.
+       ;;
+       ;; Example: It's not possible to defsubst a primitive, e.g. the
+       ;; following will produce an error (in any emacs flavor), since
+       ;; `nthcdr' is a primitive function that's handled specially by
+       ;; the byte compiler and thus can't be redefined:
+       ;;
+       ;;     (defsubst nthcdr (val) val)
+       ;;
+       ;; `defsubst', like `defmacro', needs to be evaluated at
+       ;; compile time, so this will produce an error during byte
+       ;; compilation.
+       ;;
+       ;; CC Mode occasionally needs to do things like this for
+       ;; cross-emacs compatibility.  It therefore uses the following
+       ;; to conditionally do a `defsubst':
+       ;;
+       ;;     (eval-when-compile
+       ;;       (if (not (fboundp 'foo))
+       ;;           (defsubst foo ...)))
+       ;;
+       ;; But `eval-when-compile' byte compiles its contents and
+       ;; _then_ evaluates it (in all current emacs versions, up to
+       ;; and including Emacs 20.6 and XEmacs 21.1 as of this
+       ;; writing).  So this will still produce an error, since the
+       ;; byte compiler will get to the defsubst anyway.  That's
+       ;; arguably a bug because the point with `eval-when-compile' is
+       ;; that it should evaluate rather than compile its contents.
+       ;;
+       ;; We get around it by expanding the body to a quoted constant
+       ;; that we eval.
+       (eval '(let ((cc-bytecomp-inside-eval-when-compile t))
+		,@body)))))
 
 (defun cc-bytecomp-setup-environment ()
   ;; Eval'ed during compilation to setup variables, functions etc
@@ -192,7 +207,7 @@ somewhat intentional."
   "A replacement for the `provide' form that restores the environment
 after the compilation.  Don't use within `eval-when-compile'."
   `(progn
-     (eval-when-compile (cc-bytecomp-restore-environment))
+     (cc-eval-when-compile (cc-bytecomp-restore-environment))
      (provide ,feature)))
 
 (defmacro cc-load (cc-part)
@@ -215,7 +230,7 @@ somewhat intentional."
 (defmacro cc-bytecomp-defvar (var)
   "Binds the symbol as a variable during compilation of the file,
 to silence the byte compiler.  Don't use within `eval-when-compile'."
-  `(eval-when-compile
+  `(cc-eval-when-compile
      (if (boundp ',var)
 	 nil
        (if (not (memq ',var cc-bytecomp-unbound-variables))
@@ -231,7 +246,7 @@ to silence the byte compiler.  Don't use within `eval-when-compile'."
 (defmacro cc-bytecomp-defun (fun)
   "Bind the symbol as a function during compilation of the file,
 to silence the byte compiler.  Don't use within `eval-when-compile'."
-  `(eval-when-compile
+  `(cc-eval-when-compile
      (if (not (assq ',fun cc-bytecomp-original-functions))
 	 (setq cc-bytecomp-original-functions
 	       (cons (list ',fun
@@ -275,7 +290,7 @@ the file.  Don't use outside `eval-when-compile'."
 (defmacro cc-bytecomp-obsolete-var (symbol)
   "Suppress warnings about that the given symbol is an obsolete variable.
 Don't use within `eval-when-compile'."
-  `(eval-when-compile
+  `(cc-eval-when-compile
      (if (get ',symbol 'byte-obsolete-variable)
 	 (cc-bytecomp-put ',symbol 'byte-obsolete-variable nil))))
 
@@ -288,7 +303,7 @@ Don't use within `eval-when-compile'."
 (defmacro cc-bytecomp-obsolete-fun (symbol)
   "Suppress warnings about that the given symbol is an obsolete function.
 Don't use within `eval-when-compile'."
-  `(eval-when-compile
+  `(cc-eval-when-compile
      (if (eq (get ',symbol 'byte-compile) 'byte-compile-obsolete)
 	 (cc-bytecomp-put ',symbol 'byte-compile
 			  'cc-bytecomp-ignore-obsolete))))
