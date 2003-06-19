3eaa908d4dbfa44337599aa8d57d0625abea1ee3
mast <devnull@localhost>
2003-06-19 10:55:07 +0000

    #It's better to link to the last changes page than to duplicate it.

--- admin/links.h
+++ admin/links.h
@@ -2,7 +2,7 @@
 <a href="index.php">Index</a><br />
 <a href="release.php">Current release</a><br />
 <a href="cc-mode.html#Top">Browse the online docs</a><br />
-<a href="changes-latest.php">Recent changes</a><br />
+<a href="changes-530.php">Recent changes</a><br />
 <a href="installation.php">Installation notes</a><br />
 <a href="compat.php">Compatibility notes</a><br />
 <a href="lists.php">Mailing lists</a><br />
