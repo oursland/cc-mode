f43dfd99f363f60838fa818042285edff22c64c9
mast <devnull@localhost>
2003-06-19 10:53:19 +0000

    Updated for 5.30.

--- admin/index.php
+++ admin/index.php
@@ -5,16 +5,17 @@
 
 <p><strong>CC Mode is an Emacs and XEmacs mode for editing C and other
 languages with similar syntax; currently C++, Objective-C, Java,
-CORBA's IDL, and Pike. It's a standard package in both Emacs and
+CORBA's IDL, Pike, and AWK. It's a standard package in both Emacs and
 XEmacs.</strong>
 
 <p>CC Mode is brought to you by Martin Stjernholm, the current CC Mode
 maintainer.  Several people before him have helped make CC Mode what
 it is today, notably Barry Warsaw who took over of BOCM (Boring Old C
-Mode) back in 1992 and revamped it to CC Mode.
+Mode) back in 1992 and revamped it to CC Mode.  The AWK support is
+written and maintained by Alan Mackenzie.
 
 <p>The <a href="release.php">current release</a> is version
-5.28.  A list of <a href="changes-latest.php">recent changes</a> are
+5.30.  A list of <a href="changes-530.php">recent changes</a> are
 given in detail, as well as some <a
 href="installation.php">installation notes</a> and Emacs/XEmacs <a
 href="compat.php">compatibility details</a>.
--- admin/release.php
+++ admin/release.php
@@ -4,14 +4,21 @@
   include ("header.h");
 ?>
 
+<p>It's been a long time (too long) since the last CC Mode release,
+and consequently the amount of change is large - this version has
+almost twice the code size of the previous one.  Most of the new code
+concerns the new advanced font lock support for all the supported
+languages, and the AWK support contributed by Alan Mackenzie.
+Beginning with this release, there will be unannounced bugfix releases
+available with version numbers like 5.30.1, 5.30.2, etc. This is to
+improve the responsiveness to bugs.
+
 <p>The current release is available in several forms.  You can
 download the entire source distribution, including the manual in
 texinfo format as an approximately <a
-href="http://download.sourceforge.net/cc-mode/cc-mode-5.28.tar.gz">160KB
-gzip'd tarball</a> (also available <a
-href="ftp://ftp.sourceforge.net/pub/sourceforge/cc-mode/cc-mode-5.28.tar.gz">via
-FTP</a>).  You can browse the <a href="src/">individual files</a>
-too.
+href="http://download.sourceforge.net/cc-mode/cc-mode-5.30.tar.gz">300KB
+gzip'd tarball</a>.  You can browse the <a href="src/">individual
+files</a> too.
 
 <p>The documentation is also available in several forms.  You can
 either <a href="cc-mode.html#Top">browse the on-line documentation</a>
@@ -20,7 +27,7 @@ documents generated from the texinfo source, including PostScript,
 Emacs Info, and HTML versions</a>.
 
 <p>Since version 5.26, CC Mode supports using Filladapt mode to fill
-text in comments and string literals.  There are however still some <a
+text in comments and string literals.  There are however some <a
 href="filladapt.php">issues</a> with this that you should be aware of
 if you want to use Filladapt in CC Mode.
 
