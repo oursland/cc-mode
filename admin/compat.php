2bab54f43dca66d4f8bfa7097ed980b7b98bfb4d
mast <devnull@localhost>
2003-06-19 10:46:08 +0000

    ...

--- admin/compat.php
+++ admin/compat.php
@@ -5,22 +5,24 @@
 ?>
 
 <p>CC Mode should work out of the box with Emacs &gt;= 19.34, and
-XEmacs &gt;= 19.15.
+XEmacs &gt;= 19.15. This is the last version with such a high degree
+of compatibility; the next one will require the
+<code>syntax-table</code> text property which first appeared in Emacs
+20.1 and XEmacs 21.4.
 
-<p>However, the new AWK Mode requires a feature
-(<code>syntax-table</code> text properties) which first appeared in
-Emacs 20.1 and XEmacs 21.4.  If you are still using an older version
-of (X)Emacs, CC Mode will revert to using the older AWK Mode which is
-distributed with (X)Emacs (from file <code>awk-mode.elc</code>) for
-AWK files, even if you have an autoload form that loads
-<code>cc-mode</code> for <code>awk-mode</code> in your
-<code>.emacs</code> or <code>init.el</code> file.  Upgrading your
-(X)Emacs is strongly recommended if this is the case.  You can check
-which AWK Mode you are running by displaying the mode documentation
-string with C-h m from an AWK buffer.  The newer mode's doc string
-contains <code>"to submit a problem report, enter `C-c C-b'"</code>
-near the top of the doc string where the older mode has <code>"This is
-much like C mode except ...."</code>.
+<p>The new AWK Mode already requires the <code>syntax-table</code>
+feature.  If you are still using an older version of (X)Emacs, CC Mode
+will revert to using the older AWK Mode which is distributed with
+(X)Emacs (from file <code>awk-mode.elc</code>) for AWK files, even if
+you have an autoload form that loads <code>cc-mode</code> for
+<code>awk-mode</code> in your <code>.emacs</code> or
+<code>init.el</code> file.  Upgrading your (X)Emacs is strongly
+recommended if this is the case.  You can check which AWK Mode you are
+running by displaying the mode documentation string with C-h m from an
+AWK buffer.  The newer mode's doc string contains <code>"to submit a
+problem report, enter `C-c C-b'"</code> near the top of the doc string
+where the older mode has <code>"This is much like C mode except
+...."</code>.
 
 <p>If you're using Emacs 21.3 or earlier, or XEmacs 21.3 or earlier,
 then the file <code>cc-fix.el(c)</code> is needed and will be loaded
