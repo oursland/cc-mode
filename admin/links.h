d47880a2cdf4a29712b75ffc90ca990b71e32145
mast <devnull@localhost>
2003-06-19 12:12:46 +0000

    Avoid line breaks.

--- admin/links.h
+++ admin/links.h
@@ -1,10 +1,10 @@
 <!-- -*- html -*- -->
 <a href="index.php">Index</a><br />
-<a href="release.php">Current release</a><br />
-<a href="html-manual/index.html">Browse the online docs</a><br />
-<a href="changes-530.php">Recent changes</a><br />
-<a href="installation.php">Installation notes</a><br />
-<a href="compat.php">Compatibility notes</a><br />
-<a href="lists.php">Mailing lists</a><br />
-<a href="anoncvs.php">Anonymous CVS</a><br />
-<a href="contrib.php">Contributions and links</a><br />
+<a href="release.php">Current&nbsp;release</a><br />
+<a href="html-manual/index.html">Browse&nbsp;the&nbsp;online&nbsp;docs</a><br />
+<a href="changes-530.php">Recent&nbsp;changes</a><br />
+<a href="installation.php">Installation&nbsp;notes</a><br />
+<a href="compat.php">Compatibility&nbsp;notes</a><br />
+<a href="lists.php">Mailing&nbsp;lists</a><br />
+<a href="anoncvs.php">Anonymous&nbsp;CVS</a><br />
+<a href="contrib.php">Contributions&nbsp;and&nbsp;links</a><br />
