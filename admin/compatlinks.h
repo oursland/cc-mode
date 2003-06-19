8cec7f7a3dfb8f11ab840c4015e5d1fedad7d7df
mast <devnull@localhost>
2003-06-19 10:45:57 +0000

    The compat issues is now on a single page.

--- admin/compatlinks.h
+++
@@ -1,6 +0,0 @@
-<!-- -*- html -*- -->
-<h4>Compatibility</h4>
-<a href="compat_xemacs.php">XEmacs, Emacs &gt;= 20</a><br />
-<a href="compat_emacs19.php">Emacs 19</a><br />
-<a href="compat_emacs18.php">Emacs 18</a><br />
-<a href="compat_delbs_awk.php">Other Elisp</a><br />
