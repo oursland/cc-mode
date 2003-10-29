da5c4c44e1edcc33c0356559285a16c5972f7fb8
mast <devnull@localhost>
2003-10-29 21:20:10 +0000

    (c-beginning-of-inheritance-list): Cope with fully qualified identifiers containing "::".

--- cc-engine.el
+++ cc-engine.el
@@ -4199,7 +4199,7 @@ This function does not do any hidden buffer changes."
     (c-with-syntax-table c++-template-syntax-table
       (c-backward-token-2 0 t lim)
       (while (and (or (looking-at c-symbol-start)
-		      (looking-at "[<,]"))
+		      (looking-at "[<,]\\|::"))
 		  (zerop (c-backward-token-2 1 t lim))))
       (skip-chars-forward "^:"))))
 
