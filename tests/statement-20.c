63785122196ad9a99c2013cddc703fc04b4a3205
mast <devnull@localhost>
2003-11-16 15:02:32 +0000

    Changed the policy for always recognizing unknown block constructs as nested defuns.  substatement-open is used instead, which still avoids getting involuntary gnu style indentation for such blocks (although it doesn't show in the test style).

--- tests/statement-20.c
+++ tests/statement-20.c
@@ -1,7 +1,3 @@
-/* NEW_MAPPING_LOOP below is a macro.  It's recognized as a nested
- * defun, which while not really correct still gives sane indentation.
- * (Besides, for all we know the macro could expand to to a defun as
- * well.) */
 void x()
 {
     if (sp[-1].type == T_MAPPING)
@@ -20,7 +16,7 @@ void z()
 {
     if (sp[-1].type == T_MAPPING)
 	NEW_MAPPING_LOOP (sp[-1].u.mapping->md)
-	{
-	    foo();
-	}
+	    {
+		foo();
+	    }
 }
