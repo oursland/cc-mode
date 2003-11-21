6af1e2e1c07220a6b2cf0e96a09cebe00f793dac
mast <devnull@localhost>
2003-11-21 05:57:20 +0000

    Test Qt style arbitrary access labels.

---
+++ tests/access-labels-qt-1.cc
@@ -0,0 +1,13 @@
+class MyWidget
+{
+public:
+    void aFunction();
+public slots:
+    void doSomething();
+protected slots:
+    void doSomethingElse();
+signals:
+    void changed();
+more slots:
+    int foo;
+};
