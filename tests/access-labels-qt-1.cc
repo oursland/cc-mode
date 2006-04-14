49fd8db93d9d92c94e476f3aef182265561ca463
acmacm <devnull@localhost>
2006-04-14 09:07:22 +0000

    Jazz up the QT "slots:" declarations by inserting comments and macros into them.

--- tests/access-labels-qt-1.cc
+++ tests/access-labels-qt-1.cc
@@ -2,10 +2,16 @@ class MyWidget
 {
 public:
     void aFunction();
-public slots:
+public
+#define foo "FOO" 
+slots
+#define bar "BAR"
+:
     void doSomething();
-protected slots:
+protected /* comment */ slots:
     void doSomethingElse();
+private slots:
+    void doSomethingPrivate ();
 signals:
     void changed();
 more slots:
