27ce8ec417c89841ee611f36eda19eba309e23ad
acmacm <devnull@localhost>
2007-09-22 20:44:15 +0000

    At an extra link to directory "manual".  Rename "Documentation" to "Manual", clarifying with "(html)" and "(info/PS/DVI)".

--- admin/links.h
+++ admin/links.h
@@ -1,7 +1,8 @@
 <!-- -*- html -*- -->
 <a href="index.php">Index</a><br />
 <a href="release.php">Download</a><br />
-<a href="html-manual/index.html">Documentation</a><br />
+<a href="html-manual/index.html">Manual&nbsp;(html)</a><br />
+<a href="manual">Manual&nbsp;(info/PS/DVI)</a><br />
 <a href="changes-531.php">Recent&nbsp;changes</a><br />
 <a href="installation.php">Installation</a><br />
 <a href="compat.php">Compatibility</a><br />
