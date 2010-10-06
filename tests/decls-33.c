0bd11371a976cc64c118eadcd5ee57df6f253ef5
acmacm <devnull@localhost>
2010-10-06 16:15:31 +0000

    CC Mode now correctly fontifies declarators according to the presence/absence of "typedef".  Adapt the test files accordingly.

--- tests/decls-33.c
+++ tests/decls-33.c
@@ -1,5 +1,5 @@
 void f() {
     BEGIN_AGGREGATE_ARRAY (MINIMUM (a->size, 120)) {
       foo();
-    } END_AGGREGATE_ARRAY;	/* Known bogus fontification. */
+    } END_AGGREGATE_ARRAY;	/* Known previously bogus fontification. */
 }
