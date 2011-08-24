d0ce0669696db320f7199c3a5dfb0a89549254fa
acmacm <devnull@localhost>
2011-08-24 19:32:39 +0000

    Update the "recent changes" pointer to 5.32.

--- admin/links.h
+++ admin/links.h
@@ -3,7 +3,7 @@
 <a href="release.php">Download</a><br />
 <a href="html-manual/index.html">Manual&nbsp;(html)</a><br />
 <a href="manual">Manual&nbsp;(info/PS/DVI)</a><br />
-<a href="changes-531.php">Recent&nbsp;changes</a><br />
+<a href="changes-532.php">Recent&nbsp;changes</a><br />
 <a href="installation.php">Installation</a><br />
 <a href="compat.php">Compatibility</a><br />
 <a href="lists.php">Mailing&nbsp;lists</a><br />
