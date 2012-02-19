911b9a40f918e96e94b5bedfeb758dd64e6dae87
Alan Mackenzie <bug-cc-mode@gnu.org>
2012-02-19 17:21:41 +0000

    Modify the web pages for recent conversion to Mercurial.
    hgaccess.php: New file.
    anoncvs.php: Mark old CVS repository as obsolete, change to past tense.
    index.php: Direct the adventurous to hg rather than cvs.
    links.h: Add new links for hg pages.  Move cvs pages to new "obsolete"
    section.

--- admin/anoncvs.php
+++ admin/anoncvs.php
@@ -4,13 +4,12 @@
   include ("header.h");
 ?>
 
-<p>CC Mode is available through anonymous CVS.  You can get the latest
-development version and also (since version 5.30) the fixes that have
-been incorporated into the latest release, i.e. what will become
-5.32.1, 5.32.2 and so on.  You can browse the CVS repository <a
-href="http://cc-mode.cvs.sourceforge.net/cc-mode/cc-mode">here</a>.
+<p>CC Mode used to be developed under CVS.  You can get the last CVS
+development version, just before the conversion to Mercurial in Febrary 2012.
+You can also browse this historical CVS
+repository <a href="http://cc-mode.cvs.sourceforge.net/cc-mode/cc-mode">here</a>.
 
-<p>To get the latest release with only bug fixes and no new features,
+<p>To get the final release branch with only bug fixes and no new features,
 check out the patch branch, which is called <i>Branch_5_32</i>:
 
 <pre>
@@ -21,36 +20,25 @@ cvs -d:pserver:anonymous@cc-mode.cvs.sourceforge.net:/cvsroot/cc-mode login</pre
 <pre>
 cvs -z3 -d:pserver:anonymous@cc-mode.cvs.sourceforge.net:/cvsroot/cc-mode co -rBranch_5_32 cc-mode</pre>
 
-<p>It should always be safe to use the latest version on this branch.
-Please report any problems.
+<h3>Checking out the final development version</h3>
 
-<h3>Checking out the development version</h3>
-
-<p>The development version is on the main trunk in cvs:
+<p>The development version was on the main trunk in cvs:
 
 <pre>
 cvs -z3 -d:pserver:anonymous@cc-mode.cvs.sourceforge.net:/cvsroot/cc-mode co cc-mode</pre>
 
-<p>Being under development, there are of course no guarantees that this
-version will work all the time, or at all.  That said, it usually works well;
-at least I (Alan) use it in my daily work, not only when I hack on it.  It
-might not be entirely compatible with user settings, but it can get more
-advanced fixes that are considered too risky to be allowed into the patch
-branch.
-
-<p>You are especially welcome to report bugs, opinions and patches
-regarding the development version.  However if you've found a bug,
-it's a good idea to try an update before you report it since chances
-are that we have found it ourselves already.
+<p>Please don't report any bugs you might find in these versions.  Instead,
+clone the latest Mercurial <a href="hgaccess.php">repository</a> and report
+the bugs which are there.  :-)
 
 <h3>The source tree</h3>
 
-<p>The CC Mode source will be in the root of the checked out tree.
-It's the same thing you'll find in a dist tarball (with a couple of
-extra files around it), so it can be byte compiled and used straight
-away.  You'll find the regression test suite in the <code>tests</code>
-directory; take a look in <code>000tests.el</code> to figure out how
-to use it.  The <code>admin</code> directory probably isn't very
-interesting; it just contains the source for this web site.
+<p>The CC Mode source would have been in the root of the checked out tree.
+It's the same thing you'd find in a dist tarball (with a couple of extra
+files around it), so it could have been byte compiled and used straight away.
+You'd have found the regression test suite in the <code>tests</code>
+directory; look in <code>000tests.el</code> to figure out how to use it.
+The <code>admin</code> directory probably wasn't very interesting; it just
+contained the source for this web site.
 
 <?php include ("footer.h"); ?>
--- admin/links.h
+++ admin/links.h
@@ -7,7 +7,10 @@
 <a href="installation.php">Installation</a><br />
 <a href="compat.php">Compatibility</a><br />
 <a href="lists.php">Mailing&nbsp;lists</a><br />
-<a href="anoncvs.php">Anonymous&nbsp;CVS</a><br />
-<a href="http://cc-mode.cvs.sourceforge.net/cc-mode/cc-mode">Browse&nbsp;CVS&nbsp;Repository</a><br />
+<a href="hgaccess.php">Mercurial&nbsp;Repository</a><br />
+<a href="http://cc-mode.hg.sourceforge.net:8000/hgroot/cc-mode/cc-mode">Browse&nbsp;hg&nbsp;Repository</a><br />
 <a href="contrib.php">Contributions&nbsp;and&nbsp;links</a><br />
 <a href="https://sourceforge.net/projects/cc-mode/">SourceForge&nbsp;project&nbsp;page</a><br />
+<p>
+<a href="anoncvs.php">Anonymous&nbsp;CVS</a><br />
+<a href="http://cc-mode.cvs.sourceforge.net/cc-mode/cc-mode">Browse&nbsp;old&nbsp;CVS&nbsp;Repository</a><br />
