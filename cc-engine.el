33b9f720272c080627dcd3cd140a25e3ba57aafa
Alan Mackenzie <bug-cc-mode@gnu.org>
2012-09-09 11:14:04 +0000

    Correct the handling of two c-state-cache state variables. cc-engine.el (c-state-cache-init): Initialise c-state-semi-nonlit-pos-cache\(-limit\)? properly. (c-record-parse-state-state): record c-state-semi-nonlit-pos-cache\(-limit\)?.

--- cc-engine.el
+++ cc-engine.el
@@ -3094,6 +3094,8 @@ comment at the start of cc-engine.el for more info."
 	c-state-cache-good-pos 1
 	c-state-nonlit-pos-cache nil
 	c-state-nonlit-pos-cache-limit 1
+	c-state-semi-nonlit-pos-cache nil
+	c-state-semi-nonlit-pos-cache-limit 1
 	c-state-brace-pair-desert nil
 	c-state-point-min 1
 	c-state-point-min-lit-type nil
@@ -3363,6 +3365,8 @@ comment at the start of cc-engine.el for more info."
 	   c-state-cache-good-pos
 	   c-state-nonlit-pos-cache
 	   c-state-nonlit-pos-cache-limit
+	   c-state-semi-nonlit-pos-cache
+	   c-state-semi-nonlit-pos-cache-limit
 	   c-state-brace-pair-desert
 	   c-state-point-min
 	   c-state-point-min-lit-type
