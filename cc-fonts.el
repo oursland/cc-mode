67b585fff0f679723ce598cc947036af5e587706
Alan Mackenzie <bug-cc-mode@gnu.org>
2013-05-04 15:45:55 +0000

    cc-defs.el (c-version): increment to 5.32.5 *.el, cc-mode.texi.  Add 2013 to the copyright statements.

--- cc-fonts.el
+++ cc-fonts.el
@@ -1,7 +1,7 @@
 ;;; cc-fonts.el --- font lock support for CC Mode
 
 ;; Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009,
-;;   2010, 2011, 2012  Free Software Foundation, Inc.
+;;   2010, 2011, 2012, 2013  Free Software Foundation, Inc.
 
 ;; Authors:    2003- Alan Mackenzie
 ;;             2002- Martin Stjernholm
